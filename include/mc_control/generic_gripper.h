4a180bb87323a4a8d4408b466476a8f0f9dfc05e
Pierre Gergondet <pierre.gergondet@gmail.com>
2015-05-04 11:56:56 +0900

    Implement Gripper class

---
+++ include/mc_control/generic_gripper.h
@@ -0,0 +1,69 @@
+#ifndef _H_GENERICGRIPPER_H_
+#define _H_GENERICGRIPPER_H_
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include <mc_rbdyn/robot.h>
+
+namespace mc_control
+{
+
+struct Mimic
+{
+public:
+  Mimic();
+  Mimic(const std::string & j, double m, double o);
+public:
+  std::string joint;
+  double multiplier;
+  double offset;
+};
+
+typedef std::map<std::string, Mimic> mimic_d_t;
+
+mimic_d_t readMimic(const std::string & urdf);
+
+unsigned int findSuccessorJoint(const mc_rbdyn::Robot & robot, unsigned int bodyIndex);
+
+std::vector<std::string> gripperJoints(const mc_rbdyn::Robot & robot, const std::string & gripperName, const mimic_d_t & mimicDict);
+
+std::string findFirstCommonBody(const mc_rbdyn::Robot & robotFull, const std::string & bodyName, const mc_rbdyn::Robot & robot);
+
+struct Gripper
+{
+public:
+  Gripper(const mc_rbdyn::Robot & robot, const std::string & gripperName,
+          const mc_rbdyn::Robot & controlRobot, const std::string & robot_urdf,
+          double currentQ, double timeStep);
+
+  void setCurrentQ(double currentQ);
+
+  void setTargetQ(double targetQ);
+
+  void setTargetOpening(double targetOpening);
+
+  double curPosition() const;
+
+  const std::vector<double> & q();
+public:
+  std::vector<std::string> name;
+
+  double closeP;
+  double openP;
+  double vmax;
+  double timeStep;
+  double percentOpen;
+
+  std::vector<double> mult;
+  std::vector<double> _q;
+
+  std::string controlBodyName;
+  double targetQIn;
+  double * targetQ;
+};
+
+}
+
+#endif
---
+++ src/mc_control/generic_gripper.cpp
@@ -0,0 +1,199 @@
+#include <mc_control/generic_gripper.h>
+
+#include <cmath>
+#include <tinyxml2.h>
+
+namespace mc_control
+{
+
+Mimic::Mimic()
+: Mimic("", 0, 0)
+{
+}
+
+Mimic::Mimic(const std::string & j, double m, double o)
+: joint(j), multiplier(m), offset(o)
+{
+}
+
+mimic_d_t readMimic(const std::string & urdf)
+{
+  mimic_d_t res;
+
+  tinyxml2::XMLDocument doc;
+  doc.Parse(urdf.c_str());
+  tinyxml2::XMLElement * robot = doc.FirstChildElement("robot");
+
+  if(robot)
+  {
+    std::vector<tinyxml2::XMLElement *> joints;
+    // Extract all joint elements from the document
+    {
+      tinyxml2::XMLElement * joint = robot->FirstChildElement("joint");
+      while(joint)
+      {
+        joints.push_back(joint);
+        joint = joint->NextSiblingElement("joint");
+      }
+    }
+
+    for(auto & j : joints)
+    {
+      std::string jointName = j->Attribute("name");
+      tinyxml2::XMLElement * mimicDom = j->FirstChildElement("mimic");
+      if(mimicDom)
+      {
+        std::string j = mimicDom->Attribute("joint");
+        double m = 1;
+        mimicDom->QueryDoubleAttribute("multiplier", &m);
+        double o = 0;
+        mimicDom->QueryDoubleAttribute("offset", &o);
+
+        res[jointName] = Mimic(j, m, o);
+      }
+    }
+  }
+
+  return res;
+}
+
+unsigned int findSuccessorJoint(const mc_rbdyn::Robot & robot, unsigned int bodyIndex)
+{
+  for(size_t j = 0; j < robot.mb->nrJoints(); ++j)
+  {
+    if(robot.mb->predecessor(j) == bodyIndex)
+    {
+      return j;
+    }
+  }
+  return robot.mb->nrJoints();
+}
+
+std::vector<std::string> gripperJoints(const mc_rbdyn::Robot & robot, const std::string & gripperName, const mimic_d_t & mimicDict)
+{
+  std::vector<std::string> res;
+
+  unsigned int gripperBodyIndex = robot.bodyIndexByName(gripperName);
+  unsigned int rootBodyIndex = robot.mb->parent(gripperBodyIndex);
+  unsigned int rootJointIndex = findSuccessorJoint(robot, rootBodyIndex);
+  std::string rootJointName = robot.mb->joint(rootJointIndex).name();
+
+  res.push_back(rootJointName);
+  for(const auto & m : mimicDict)
+  {
+    if(m.second.joint == rootJointName)
+    {
+      res.push_back(m.first);
+    }
+  }
+
+  return res;
+}
+
+std::string findFirstCommonBody(const mc_rbdyn::Robot & robotFull, const std::string & bodyName, const mc_rbdyn::Robot & robot)
+{
+  if(robot.hasBody(bodyName))
+  {
+    return bodyName;
+  }
+  else
+  {
+    unsigned int bodyIndex = robotFull.bodyIndexByName(bodyName);
+    unsigned int bodyPredIndex = robotFull.mb->parent(bodyIndex);
+    if(bodyPredIndex == -1)
+    {
+      return "";
+    }
+    std::string bodyPredName = robotFull.mb->body(bodyPredIndex).name();
+    return findFirstCommonBody(robotFull, bodyPredName, robot);
+  }
+}
+
+Gripper::Gripper(const mc_rbdyn::Robot & robot, const std::string & gripperName,
+                 const mc_rbdyn::Robot & controlRobot, const std::string & robot_urdf,
+                 double currentQ, double timeStep)
+{
+  auto mimicDict = readMimic(robot_urdf);
+  name = gripperJoints(robot, gripperName, mimicDict);
+  unsigned int jointIndex = robot.jointIndexByName(name[0]);
+
+  closeP = robot.ql[jointIndex][0];
+  openP = robot.qu[jointIndex][0];
+  vmax = std::min(std::abs(robot.vl[jointIndex][0]), robot.vu[jointIndex][0])/10;
+  setCurrentQ(currentQ);
+  this->timeStep = timeStep;
+
+  mult.resize(name.size());
+  _q.resize(name.size());
+  for(size_t i = 0; i < name.size(); ++i)
+  {
+    if(i == 0)
+    {
+      mult[i] = 1;
+      _q[i] = currentQ;
+    }
+    else
+    {
+      mult[i] = mimicDict[name[i]].multiplier;
+      _q[i] = mult[i]*_q[0];
+    }
+  }
+
+  unsigned int bodyIndex = robot.mb->predecessor(jointIndex);
+  std::string bodyName = robot.mb->body(bodyIndex).name();
+  controlBodyName = findFirstCommonBody(robot, bodyName, controlRobot);
+
+  targetQIn = 0;
+  targetQ = 0;
+}
+
+void Gripper::setCurrentQ(double currentQ)
+{
+  percentOpen = (currentQ - closeP)/(openP - closeP);
+}
+
+void Gripper::setTargetQ(double targetQ)
+{
+  targetQIn = targetQ;
+  this->targetQ = &targetQIn;
+}
+
+void Gripper::setTargetOpening(double targetOpening)
+{
+  setTargetQ(curPosition() + (targetOpening - percentOpen)*(openP - closeP));
+}
+
+double Gripper::curPosition() const
+{
+  return closeP + (openP - closeP)*percentOpen;
+}
+
+const std::vector<double> & Gripper::q()
+{
+  if(targetQ)
+  {
+    if(std::abs(curPosition() - targetQIn) < 0.001)
+    {
+      targetQ = 0;
+    }
+    else
+    {
+      if(targetQIn > curPosition())
+      {
+        percentOpen += std::min(vmax*timeStep, targetQIn - curPosition())/(openP - closeP);
+      }
+      else
+      {
+        percentOpen += std::max(-vmax*timeStep, targetQIn - curPosition())/(openP - closeP);
+      }
+    }
+  }
+  _q[0] = curPosition();
+  for(size_t i = 1; i < _q.size(); ++i)
+  {
+    _q[i] = mult[i]*_q[0];
+  }
+  return _q;
+}
+
+}
