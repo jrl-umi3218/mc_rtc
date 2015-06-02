db62facc9d3da870321e66a9df74ce60dc0e9329
Pierre Gergondet <pierre.gergondet@gmail.com>
2015-06-02 14:19:52 -0700

    Implement TrajectoryTask

---
+++ include/mc_tasks/TrajectoryTask.h
@@ -0,0 +1,54 @@
+#ifndef _H_TRAJECTORYTASK_H_
+#define _H_TRAJECTORYTASK_H_
+
+#include <mc_tasks/MetaTask.h>
+#include <mc_trajectory/BSplineTrajectory.h>
+
+#include <mc_rbdyn/robot.h>
+#include <Tasks/QPTasks.h>
+
+namespace mc_tasks
+{
+
+struct TrajectoryTask : public MetaTask
+{
+public:
+  TrajectoryTask(const mc_rbdyn::Robots & robots, unsigned int robotIndex,
+                 const mc_rbdyn::Surface & surface, const sva::PTransformd & X_0_t,
+                 double duration, double timeStep, double stiffness, double posWeight, double oriWeight,
+                 const std::string & name = "TrajectoryTask",
+                 const Eigen::MatrixXd & waypoints = Eigen::MatrixXd(3,0),
+                 unsigned int nrWP = 0);
+
+  virtual void addToSolver(tasks::qp::QPSolver & solver) override;
+
+  virtual void removeFromSolver(tasks::qp::QPSolver & solver) override;
+
+  virtual void update() override;
+
+  bool timeElapsed();
+
+  const Eigen::VectorXd & eval() const;
+
+  const Eigen::VectorXd & speed() const;
+
+  std::vector<Eigen::Vector3d> controlPoints();
+
+  void generateBS();
+public:
+  const mc_rbdyn::Robots & robots;
+  const mc_rbdyn::Surface & surface;
+  sva::PTransformd X_0_t;
+  sva::PTransformd X_0_start;
+  Eigen::MatrixXd wp;
+  double duration;
+  double timeStep;
+  double t;
+  std::shared_ptr<tasks::qp::TransformTask> transTask;
+  std::shared_ptr<tasks::qp::TrajectoryTask> transTrajTask;
+  std::shared_ptr<mc_trajectory::BSplineTrajectory> bspline;
+};
+
+}
+
+#endif
