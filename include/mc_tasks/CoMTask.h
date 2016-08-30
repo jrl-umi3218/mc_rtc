5c09b442c0f697102c1ebf085e761c4444b2ec7d
Pierre Gergondet <pierre.gergondet@gmail.com>
2016-08-30 17:41:39 +0900

    Add CoMTask doxygen

--- include/mc_tasks/CoMTask.h
+++ include/mc_tasks/CoMTask.h
@@ -10,22 +10,71 @@
 namespace mc_tasks
 {
 
+/*! \brief Control a robot's CoM
+ *
+ * This task is a thin wrapper above the appropriate Tasks types
+ *
+ */
 struct MC_TASKS_DLLAPI CoMTask
 {
 public:
+  /*! \brief Constructor
+   *
+   * \param robots Robots involved in the task
+   *
+   * \param robotIndex Select the robot which CoM should be controlled
+   *
+   * \param stiffness Task stiffness
+   *
+   * \param weight Task weight
+   *
+   */
   CoMTask(const mc_rbdyn::Robots & robots, unsigned int robotIndex,
           double stifness = 5.0, double weight = 100.);
 
+  /*! \brief Reset the task
+   *
+   * Set the CoM target to the current configuration's CoM
+   *
+   * \param robots Robots involved in the task
+   *
+   * \param robotIndex Select the robot which CoM should be controlled
+   *
+   */
   void resetTask(const mc_rbdyn::Robots & robots, unsigned int robotIndex);
 
+  /*! \brief Remove the task from a solver
+   *
+   * \param solver Solver from which to remove the task
+   *
+   */
   void removeFromSolver(mc_solver::QPSolver & solver);
 
+  /*! \brief Add the task to a solver
+   *
+   * \param solver Solver where to add the task
+   *
+   */
   void addToSolver(mc_solver::QPSolver & solver);
 
+  /*! \brief Change the CoM target by a given amount
+   *
+   * \param com Modification applied to the CoM
+   *
+   */
   void move_com(const Eigen::Vector3d & com);
 
+  /*! \brief Set the CoM target to a given position
+   *
+   * \param com New CoM target
+   *
+   */
   void set_com(const Eigen::Vector3d & com);
 
+  /*! \brief Return the current CoM target
+   *
+   * \returns The current CoM target
+   */
   Eigen::Vector3d get_com();
 public:
   const mc_rbdyn::Robots & robots;
