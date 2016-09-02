356732c5d62d24021d634304af674a79101f4c77
Pierre Gergondet <pierre.gergondet@gmail.com>
2016-09-02 16:17:06 +0900

    Setup controllers' testing
    The current ticker implementation only loads the configuration. The
    implementation should be refined to:
    1. Define some expected outcome of the test (e.g. instantation will
    throw)
    2. Actually run a controller (and let this controller handle the
    internal testing)

---
+++ tests/test_controller_ticker.cpp
@@ -0,0 +1,20 @@
+#include <boost/test/unit_test.hpp>
+
+#include <mc_control/mc_global_controller.h>
+
+BOOST_AUTO_TEST_CASE(TestController)
+{
+  auto argc = boost::unit_test::framework::master_test_suite().argc;
+  auto argv = boost::unit_test::framework::master_test_suite().argv;
+  BOOST_CHECK_EQUAL(argc, 2);
+  std::string conf = argv[1];
+  //BOOST_CHECK_THROW(mc_control::MCGlobalController controller(conf), std::runtime_error);
+  try
+  {
+    mc_control::MCGlobalController controller(conf);
+  }
+  catch(const std::runtime_error & exc)
+  {
+    std::cerr << "Got " << exc.what() << std::endl;
+  }
+}
