e0e269cda1f6b665829dd4a02459c8990ca05e82
Pierre Gergondet <pierre.gergondet@gmail.com>
2017-01-30 17:58:18 +0900

    Add missing documentation in new headers

--- include/mc_rbdyn/Base.h
+++ include/mc_rbdyn/Base.h
@@ -9,11 +9,16 @@
 namespace mc_rbdyn
 {
 
+/** This struct represents a base for the creation of a robot */
 struct MC_RBDYN_DLLAPI Base
 {
+  /** Name of the base */
   std::string baseName;
+  /** Transformation from the world to the root joint */
   sva::PTransformd X_0_s;
+  /** Transformation from the root body to the root joint */
   sva::PTransformd X_b0_s;
+  /** Type of the root joint */
   rbd::Joint::Type baseType;
 };
 
