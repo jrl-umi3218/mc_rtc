bea9d2b6bc6d047bddc76b7c7c7d17e04ba1c515
Pierre Gergondet <pierre.gergondet@gmail.com>
2017-02-07 19:11:00 +0900

    Add serialization code for logging

---
+++ include/mc_control/log/serialization/fb_utils.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "MCLog_generated.h"
+
+#include <SpaceVecAlg/SpaceVecAlg>
+
+namespace mc_control
+{
+
+/** This structure allows to convert native C++ types to their flatbuffer
+ * counterpart */
+template<typename T>
+struct LogDataHelper
+{
+  /** Holds the value type corresponding to the C++ type */
+  enum { value_type = mc_control::log::LogData_NONE };
+
+  /** Convert the C++ object to an anonymous flatbuffer object */
+  static flatbuffers::Offset<void> serialize(flatbuffers::FlatBufferBuilder &,
+                                             const T &)
+  {
+    static_assert(sizeof(T) == 0, "This type is not handled by the logger interface");
+  }
+};
+
+/** This macro allows to define implementations for LogDataHelper in
+ * simple cases */
+#define IMPL_LDH(TYPE, LD_TYPE, S_FN, ARG_NAME, ...)\
+template<>\
+struct LogDataHelper<TYPE>\
+{\
+  enum { value_type = mc_control::log::LD_TYPE };\
+  static flatbuffers::Offset<void> serialize(flatbuffers::FlatBufferBuilder & builder,\
+                                             const TYPE & ARG_NAME)\
+  {\
+    return mc_control::log::S_FN(builder, __VA_ARGS__).Union();\
+  }\
+}
+
+IMPL_LDH(bool, LogData_Bool, CreateBool, b, b);
+IMPL_LDH(double, LogData_Double, CreateDouble, d, d);
+IMPL_LDH(std::vector<double>, LogData_DoubleVector, CreateDoubleVectorDirect, v, &v);
+IMPL_LDH(unsigned int, LogData_UnsignedInt, CreateUnsignedInt, i, i);
+IMPL_LDH(std::string, LogData_String, CreateStringDirect, s, s.c_str());
+IMPL_LDH(Eigen::Vector3d, LogData_Vector3d, CreateVector3d, v, v.x(), v.y(), v.z());
+IMPL_LDH(Eigen::Quaterniond, LogData_Quaterniond, CreateQuaterniond, q, q.w(), q.x(), q.y(), q.z());
+
+#undef IMPL_DATA_HELPER
+
+template<>
+struct LogDataHelper<sva::PTransformd>
+{
+  enum { value_type = mc_control::log::LogData_PTransformd };
+
+  static flatbuffers::Offset<void> serialize(flatbuffers::FlatBufferBuilder & builder,
+                                             const sva::PTransformd & pt)
+  {
+    auto q = Eigen::Quaterniond(pt.rotation());
+    auto fb_q = mc_control::log::CreateQuaterniond(builder, q.w(), q.x(), q.y(), q.z());
+    const auto & t = pt.translation();
+    auto fb_t = mc_control::log::CreateVector3d(builder, t.x(), t.y(), t.z());
+    return mc_control::log::CreatePTransformd(builder, fb_q, fb_t).Union();
+  }
+};
+
+template<typename T>
+void AddLogData(flatbuffers::FlatBufferBuilder & builder,
+                std::vector<uint8_t> & value_types,
+                std::vector<flatbuffers::Offset<void>> & values,
+                const T & value)
+{
+  value_types.push_back(LogDataHelper<T>::value_type);
+  values.push_back(LogDataHelper<T>::serialize(builder, value));
+}
+
+}
