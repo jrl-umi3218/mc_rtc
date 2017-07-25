0ee316056b4e212a68750f31791b12eea3701a46
Pierre Gergondet <pierre.gergondet@gmail.com>
2017-07-25 18:00:06 +0900

    Add test for ConstraintSetLoader

---
+++ tests/testConstraintSetLoader.cpp
@@ -0,0 +1,113 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/mpl/list.hpp>
+
+#include <mc_solver/ConstraintSetLoader.h>
+
+#include <mc_solver/BoundedSpeedConstr.h>
+
+#include <mc_rbdyn/RobotLoader.h>
+#include <mc_rbdyn/configuration_io.h>
+
+#include "utils.h"
+
+static bool configured = configureRobotLoader();
+/* Create Robots with one robot and an environment for the purpose of the test */
+static auto rm = mc_rbdyn::RobotLoader::get_robot_module("HRP2DRC");
+static auto em = mc_rbdyn::RobotLoader::get_robot_module("env",
+                                                  std::string(mc_rtc::MC_ENV_DESCRIPTION_PATH),
+                                                  std::string("ground"));
+static auto robots = mc_rbdyn::loadRobotAndEnv(*rm, *em);
+static mc_solver::QPSolver solver(robots, 0.005);
+
+template<typename T>
+struct fail : public std::false_type {};
+
+template<typename T>
+struct ConstraintTester
+{
+  static_assert(fail<T>::value, "This should be specialized");
+  mc_solver::ConstraintSetPtr make_ref() { return nullptr; }
+
+  std::string json() { return ""; }
+
+  void check(const mc_solver::ConstraintSetPtr & /*ref*/,
+             const mc_solver::ConstraintSetPtr & /*loaded*/) {}
+};
+
+template<>
+struct ConstraintTester<mc_solver::BoundedSpeedConstr>
+{
+  ConstraintTester<mc_solver::BoundedSpeedConstr>()
+  {
+    for(size_t i = 0; i < 3; ++i)
+    {
+      if(lS(i) > 0) { lS(i) = -lS(i); }
+      uS(i) = -lS(i);
+    }
+  }
+
+  mc_solver::ConstraintSetPtr make_ref()
+  {
+    auto ret = std::make_shared<mc_solver::BoundedSpeedConstr>(*robots, 0, solver.dt());
+    ret->addBoundedSpeed(solver, "RARM_LINK6", Eigen::Vector3d::Zero(), Eigen::Matrix6d::Identity(), s);
+    ret->addBoundedSpeed(solver, "LARM_LINK6", Eigen::Vector3d::Zero(), Eigen::Matrix6d::Identity(), lS, uS);
+    return ret;
+  }
+
+  std::string json()
+  {
+    mc_rtc::Configuration config;
+    config.add("type", "boundedSpeed");
+    config.add("robotIndex", 0);
+    auto cs = config.array("constraints");
+    {
+      mc_rtc::Configuration c;
+      c.add("body", "RARM_LINK6");
+      c.add("speed", s);
+      cs.push(c);
+    }
+    {
+      mc_rtc::Configuration c;
+      c.add("body", "LARM_LINK6");
+      c.add("lowerSpeed", lS);
+      c.add("upperSpeed", lS);
+      cs.push(c);
+    }
+    {
+      // No speed entry so shouldn't matter
+      mc_rtc::Configuration c;
+      c.add("body", "RARM_LINK6");
+      cs.push(c);
+    }
+    auto ret = getTmpFile();
+    config.save(ret);
+    return ret;
+  }
+
+  void check(const mc_solver::ConstraintSetPtr & ref_p,
+             const mc_solver::ConstraintSetPtr & loaded_p)
+  {
+    auto ref = std::dynamic_pointer_cast<mc_solver::BoundedSpeedConstr>(ref_p);
+    auto loaded = std::dynamic_pointer_cast<mc_solver::BoundedSpeedConstr>(loaded_p);
+    BOOST_REQUIRE(ref);
+    BOOST_REQUIRE(loaded);
+    BOOST_CHECK(ref->nrBoundedSpeeds() == loaded->nrBoundedSpeeds());
+    BOOST_CHECK(loaded->removeBoundedSpeed(solver, "RARM_LINK6"));
+    BOOST_CHECK(loaded->removeBoundedSpeed(solver, "LARM_LINK6"));
+  }
+
+  Eigen::Vector3d s = Eigen::Vector3d::Random();
+  Eigen::Vector3d lS = Eigen::Vector3d::Random();
+  Eigen::Vector3d uS = Eigen::Vector3d::Zero();
+};
+
+typedef boost::mpl::list<mc_solver::BoundedSpeedConstr> test_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(TestConstraintSetLoader, T, test_types)
+{
+  auto tester = ConstraintTester<T>();
+  auto ref = tester.make_ref();
+  auto conf = tester.json();
+  auto loaded = mc_solver::ConstraintSetLoader::load(solver, conf);
+  tester.check(ref, loaded);
+}
