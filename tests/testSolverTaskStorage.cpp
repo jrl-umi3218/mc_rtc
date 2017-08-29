eb8503a225d7d6e3784d4d7a6c5230d9b6b63f93
Pierre Gergondet <pierre.gergondet@gmail.com>
2017-08-29 11:06:15 +0900

    Add a test for tasks' shared_ptr life extension in QPSolver

---
+++ tests/testSolverTaskStorage.cpp
@@ -0,0 +1,34 @@
+#include <boost/test/unit_test.hpp>
+
+#include <mc_rbdyn/RobotLoader.h>
+#include <mc_rbdyn/Robots.h>
+#include <mc_solver/QPSolver.h>
+#include <mc_tasks/CoMTask.h>
+
+/** This test verifies that task's life is properly extended by the solver */
+
+/** Normal CoMTask with static boolean to check the object is deleted */
+struct CheckCoMTask : public mc_tasks::CoMTask
+{
+  using mc_tasks::CoMTask::CoMTask;
+  ~CheckCoMTask() { deleted = true; }
+  static bool deleted;
+};
+
+bool CheckCoMTask::deleted = false;
+
+BOOST_AUTO_TEST_CASE(TestSolverTaskStorage)
+{
+  auto rm = mc_rbdyn::RobotLoader::get_robot_module("HRP2DRC");
+  auto robots = mc_rbdyn::loadRobot(*rm);
+  mc_solver::QPSolver solver(robots, 0.005);
+  CheckCoMTask * t_ptr = nullptr;
+  {
+    auto t = std::make_shared<CheckCoMTask>(solver.robots(), 0);
+    t_ptr = t.get();
+    solver.addTask(t);
+  }
+  BOOST_REQUIRE(CheckCoMTask::deleted == false);
+  solver.removeTask(t_ptr);
+  BOOST_REQUIRE(CheckCoMTask::deleted == true);
+}
