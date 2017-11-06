064b062ddf4eac658ddf115443753cc8847ce3cc
Pierre Gergondet <pierre.gergondet@gmail.com>
2017-11-06 18:27:52 +0900

    Fix HRP4 RobotModule related to recent changes

--- src/mc_robots/hrp4.cpp
+++ src/mc_robots/hrp4.cpp
@@ -330,24 +330,25 @@ namespace mc_robots
     readUrdf("hrp4", filteredLinks);
 
     _springs.springsBodies = { "l_ankle", "r_ankle" }; //TODO: check these are the correct bodies
+
+    auto fileByBodyName = stdCollisionsFiles(mb);
+    _convexHull = getConvexHull(fileByBodyName);
+    _bounds = nominalBounds(limits);
+    _stance = halfSittingPose(mb);
   }
 
   const std::map<std::string, std::pair<std::string, std::string> > & HRP4NoHandRobotModule::convexHull() const
   {
-    auto fileByBodyName = stdCollisionsFiles(mb);
-    const_cast<HRP4NoHandRobotModule*>(this)->_convexHull = getConvexHull(fileByBodyName);
     return _convexHull;
   }
 
   const std::vector< std::map<std::string, std::vector<double> > > & HRP4NoHandRobotModule::bounds() const
   {
-    const_cast<HRP4NoHandRobotModule*>(this)->_bounds = nominalBounds(limits);
     return _bounds;
   }
 
   const std::map<std::string, std::vector<double> > & HRP4NoHandRobotModule::stance() const
   {
-    const_cast<HRP4NoHandRobotModule*>(this)->_stance = halfSittingPose(mb);
     return _stance;
   }
 
@@ -356,24 +357,24 @@ namespace mc_robots
     readUrdf("hrp4", filteredLinks);
 
     _springs.springsBodies = { "l_ankle", "r_ankle" }; //TODO: check these are the correct bodies
+    auto fileByBodyName = stdCollisionsFiles(mb);
+    _convexHull = getConvexHull(fileByBodyName);
+    _bounds = nominalBounds(limits);
+    _stance = halfSittingPose(mb);
   }
 
   const std::map<std::string, std::pair<std::string, std::string> > & HRP4WithHandRobotModule::convexHull() const
   {
-    auto fileByBodyName = stdCollisionsFiles(mb);
-    const_cast<HRP4WithHandRobotModule*>(this)->_convexHull = getConvexHull(fileByBodyName);
     return _convexHull;
   }
 
   const std::vector< std::map<std::string, std::vector<double> > > & HRP4WithHandRobotModule::bounds() const
   {
-    const_cast<HRP4WithHandRobotModule*>(this)->_bounds = nominalBounds(limits);
     return _bounds;
   }
 
   const std::map<std::string, std::vector<double> > & HRP4WithHandRobotModule::stance() const
   {
-    const_cast<HRP4WithHandRobotModule*>(this)->_stance = halfSittingPose(mb);
     return _stance;
   }
 
