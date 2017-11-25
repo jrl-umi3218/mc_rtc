a57ce8b55ae81fe173e668b69beb09427b165d3c
Stéphane Caron <stephane.caron@normalesup.org>
2017-11-25 11:43:14 +0100

    Improvement from @gergondet

--- src/mc_rtc/ros.cpp
+++ src/mc_rtc/ros.cpp
@@ -230,7 +230,7 @@ private:
           tf_caster.sendTransform(msg.tfs);
           for (const auto & wrench : msg.wrenches)
           {
-            const std::string & sensor_name = wrench.header.frame_id.substr(6); // remove "calib/" prefix
+            const std::string & sensor_name = wrench.header.frame_id.substr(strlen("calib/")); // remove "calib/" prefix
             if (wrenches_pub.count(sensor_name) == 0)
             {
               wrenches_pub.insert({
