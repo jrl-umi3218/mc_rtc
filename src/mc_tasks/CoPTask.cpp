c556769f2cada44dab3c1b5dc015ea048166010b
Stéphane Caron <stephane.caron@lirmm.fr>
2017-12-06 21:07:53 +0100

    :lipstick: JSON file

--- src/mc_tasks/CoPTask.cpp
+++ src/mc_tasks/CoPTask.cpp
@@ -39,11 +39,12 @@ static bool registered = mc_tasks::MetaTaskLoader::register_load_function("cop",
      const mc_rtc::Configuration & config)
   {
     auto t = std::make_shared<mc_tasks::CoPTask>(config("surface"), solver.robots(), config("robotIndex"), solver.dt());
+    if(config.has("admittance")) { t->admittance(config("admittance")); }
+    if(config.has("cop")) { t->targetCoP(config("cop")); }
+    if(config.has("force")) { t->targetForce(config("force")); }
+    if(config.has("pose")) { t->targetPose(config("pose")); }
     if(config.has("stiffness")) { t->stiffness(config("stiffness")); }
     if(config.has("weight")) { t->weight(config("weight")); }
-    if(config.has("admittance")) { t->admittance(config("admittance")); }
-    if(config.has("pose")) { t->targetPose(config("pose")); }
-    if(config.has("wrench")) { t->targetWrench(config("wrench")); }
     t->load(solver, config);
     return t;
   }
