d3ab3a55051f94ee2c4c87146241c1a5c964189b
Pierre Gergondet <pierre.gergondet@gmail.com>
2018-06-06 10:07:18 +0900

    Revert changes to CoM sample controller

--- src/mc_control/samples/CoM/mc_com_controller.cpp
+++ src/mc_control/samples/CoM/mc_com_controller.cpp
@@ -16,11 +16,8 @@ MCCoMController::MCCoMController(std::shared_ptr<mc_rbdyn::RobotModule> robot_mo
 {
   qpsolver->addConstraintSet(contactConstraint);
   qpsolver->addConstraintSet(dynamicsConstraint);
-  //qpsolver->addConstraintSet(kinematicsConstraint);
   qpsolver->addConstraintSet(selfCollisionConstraint);
-  auto pt = std::make_shared<mc_tasks::PostureTask>(solver(), 0, 2.0, 1.0);
-  solver().addTask(pt);
-  //qpsolver->addTask(postureTask.get());
+  qpsolver->addTask(postureTask);
 
   comTask.reset(new mc_tasks::CoMTask(robots(), robots().robotIndex()));
   postureTask->stiffness(1);
@@ -35,34 +32,17 @@ void MCCoMController::reset(const ControllerResetData & reset_data)
   solver().addTask(comTask);
   if(robot().name() == "hrp2_drc")
   {
-    mc_rbdyn::Contact c{robots(), "LeftFingers", "AllGround"};
-    auto id = c.contactId(robots());
-    Eigen::Matrix6d dof = Eigen::Matrix6d::Identity();
-    dof(5,5) = 0;
     qpsolver->setContacts({
       mc_rbdyn::Contact(robots(), "LFullSole", "AllGround"),
-      mc_rbdyn::Contact(robots(), "RFullSole", "AllGround"),
-      c
+      mc_rbdyn::Contact(robots(), "RFullSole", "AllGround")
     });
-    contactConstraint.contactConstr->addDofContact(id, dof);
-    contactConstraint.contactConstr->updateDofContacts();
   }
   else if(robot().name() == "hrp4")
   {
-    mc_rbdyn::Contact c{robots(), "LeftHand", "AllGround"};
-    auto id = c.contactId(robots());
-    Eigen::Matrix6d dof = Eigen::Matrix6d::Identity();
-    dof(5,5) = 0;
-    contactConstraint.contactConstr->addDofContact(id, dof);
-    contactConstraint.contactConstr->updateDofContacts();
     qpsolver->setContacts({
       mc_rbdyn::Contact(robots(), "LeftFoot", "AllGround"),
       mc_rbdyn::Contact(robots(), "RightFoot", "AllGround"),
-      //c
     });
-    auto t = std::make_shared<mc_tasks::SurfaceTransformTask>("LeftHand", robots(), 0);
-    //t->target(sva::PTransformd(Eigen::Vector3d{0, 0, 0.1}) * t->target());
-    solver().addTask(t);
   }
   else
   {
