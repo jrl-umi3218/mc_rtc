39c531b7eed30d2777fd16fe1c418f20fbafd67b
Pierre Gergondet <pierre.gergondet@gmail.com>
2018-10-19 10:56:02 +0900

    [FSM] Add an option to delay the start of some states in Parallel

--- include/mc_control/fsm/states/Parallel.h
+++ include/mc_control/fsm/states/Parallel.h
@@ -19,8 +19,17 @@ namespace fsm
  * Configuration entries:
  *
  * - states: list of states run by this state
+ * - delays: delay the start of the given state(s) by the given time (in seconds)
  * - configs: for each state in states, configs(state) is used to further
  *   configure state
+ *
+ * Example:
+ *
+ * {
+ *   "states": [ "StateA", "StateB", "StateC" ], // Play StateA, StateB and StateC
+ *   "delays": { "StateA": 1.0 }, // StateA will start one second after StateB and StateC
+ *   "configs: { "StateB": {} }
+ * }
  *
  */
 
@@ -42,7 +51,21 @@ struct MC_CONTROL_FSM_STATE_DLLAPI ParallelState : State
 
 protected:
   mc_rtc::Configuration config_;
-  std::vector<StatePtr> states_;
+  double time_ = 0;
+  struct DelayedState
+  {
+    DelayedState(Controller & ctl, const std::string & name, double delay, mc_rtc::Configuration config);
+    bool run(Controller & ctl, double time);
+    StatePtr & state();
+
+  private:
+    StatePtr state_;
+    std::string name_;
+    mc_rtc::Configuration config_;
+    double delay_;
+    void createState(Controller & ctl);
+  };
+  std::vector<DelayedState> states_;
 };
 
 } // namespace fsm
--- src/mc_control/fsm/states/Parallel.cpp
+++ src/mc_control/fsm/states/Parallel.cpp
@@ -7,6 +7,41 @@ namespace mc_control
 namespace fsm
 {
 
+ParallelState::DelayedState::DelayedState(Controller & ctl,
+                                          const std::string & name,
+                                          double delay,
+                                          mc_rtc::Configuration config)
+: state_(nullptr), name_(name), config_(config), delay_(delay)
+{
+  if(delay == 0)
+  {
+    createState(ctl);
+  }
+}
+
+bool ParallelState::DelayedState::run(Controller & ctl, double time)
+{
+  if(state_)
+  {
+    return state_->run(ctl);
+  }
+  if(time > delay_)
+  {
+    createState(ctl);
+  }
+  return false;
+}
+
+StatePtr & ParallelState::DelayedState::state()
+{
+  return state_;
+}
+
+void ParallelState::DelayedState::createState(Controller & ctl)
+{
+  state_ = ctl.factory().create(name_, ctl, config_);
+}
+
 void ParallelState::configure(const mc_rtc::Configuration & config)
 {
   config_.load(config);
@@ -20,10 +55,10 @@ void ParallelState::start(Controller & ctl)
     LOG_ERROR_AND_THROW(std::runtime_error, "ParallelState requires at least one state to run")
   }
   auto states_config = config_("configs", mc_rtc::Configuration{});
-  states_.reserve(states.size());
+  auto delays = config_("delays", mc_rtc::Configuration{});
   for(auto & s : states)
   {
-    states_.emplace_back(ctl.factory().create(s, ctl, states_config(s, mc_rtc::Configuration{})));
+    states_.emplace_back(ctl, s, delays(s, 0.0), states_config(s, mc_rtc::Configuration{}));
   }
 }
 
@@ -32,11 +67,12 @@ bool ParallelState::run(Controller & ctl)
   bool ret = true;
   for(auto & s : states_)
   {
-    ret = s->run(ctl) && ret;
+    ret = s.run(ctl, time_) && ret;
   }
+  time_ += ctl.solver().dt();
   if(ret)
   {
-    output(states_.back()->output());
+    output(states_.back().state()->output());
   }
   return ret;
 }
@@ -45,7 +81,10 @@ void ParallelState::stop(Controller & ctl)
 {
   for(auto & s : states_)
   {
-    s->stop(ctl);
+    if(s.state())
+    {
+      s.state()->stop(ctl);
+    }
   }
 }
 
@@ -53,7 +92,10 @@ void ParallelState::teardown(Controller & ctl)
 {
   for(auto & s : states_)
   {
-    s->teardown_(ctl);
+    if(s.state())
+    {
+      s.state()->teardown_(ctl);
+    }
   }
 }
 
@@ -61,7 +103,7 @@ bool ParallelState::read_msg(std::string & msg)
 {
   for(auto & s : states_)
   {
-    if(s->read_msg(msg))
+    if(s.state() && s.state()->read_msg(msg))
     {
       return true;
     }
@@ -73,7 +115,7 @@ bool ParallelState::read_write_msg(std::string & msg, std::string & out)
 {
   for(auto & s : states_)
   {
-    if(s->read_write_msg(msg, out))
+    if(s.state() && s.state()->read_write_msg(msg, out))
     {
       return true;
     }
