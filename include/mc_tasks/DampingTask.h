1315830896a8507d406189312b72c9e29c688801
Kevin Chappellet <kevin.chappellet@gmail.com>
2018-10-30 17:43:02 +0100

    Rename current master AdmittanceTask to DampingTask

---
+++ include/mc_tasks/DampingTask.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <mc_tasks/AdmittanceTask.h>
+
+namespace mc_tasks
+{
+
+namespace force
+{
+
+/*! \brief Hybrid position-force control on a contacting end-effector.
+ *
+ * The DampingTask is by default a SurfaceTransformTask, i.e. pure position
+ * control of a surface frame. Admittance coefficients that map force errors to
+ * displacements (see [1] and [2]) are initially set to zero.
+ *
+ * When the admittance along one axis (Fx, Fy, Fz, Tx, Ty or Tz) is set to a
+ * non-zero positive value, this axis switches from position to force control.
+ * The goal is then to realize the prescribed target wrench at the surface
+ * frame (bis repetita placent: wrenches are expressed in the surface frame of
+ * the task, not in the sensor frame of the corresponding body). The force
+ * control law applied is damping control [3].
+ *
+ * See the discussion in [4] for a comparison with the ComplianceTask.
+ *
+ * [1] https://en.wikipedia.org/wiki/Mechanical_impedance
+ * [2] https://en.wikipedia.org/wiki/Impedance_analogy
+ * [3] https://doi.org/10.1109/IROS.2010.5651082
+ * [4] https://gite.lirmm.fr/multi-contact/mc_rtc/issues/34
+ *
+ */
+struct MC_TASKS_DLLAPI DampingTask : AdmittanceTask 
+{
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  /*! \brief Initialize a new admittance task.
+   *
+   * \param robotSurface Name of the surface frame to control, in which the
+   * desired wrench will also be expressed
+   *
+   * \param robots Robots where the task will be applied
+   *
+   * \param robotIndex Which robot among the robots
+   *
+   * \param stiffness Stiffness of the underlying SurfaceTransform task
+   *
+   * \param weight Weight of the underlying SurfaceTransform task
+   *
+   * \throws If the body the task is attempting to control does not have a
+   * sensor attached to it
+   *
+   */
+  DampingTask(const std::string & robotSurface,
+                 const mc_rbdyn::Robots & robots,
+                 unsigned robotIndex,
+                 double stiffness = 5.0,
+                 double weight = 1000.0);
+
+protected:
+
+  void update() override;
+
+};
+
+} // namespace force
+
+} // namespace mc_tasks
+
---
+++ src/mc_tasks/DampingTask.cpp
@@ -0,0 +1,84 @@
+#include <mc_rbdyn/configuration_io.h>
+#include <mc_rbdyn/rpy_utils.h>
+#include <mc_tasks/DampingTask.h>
+#include <mc_tasks/MetaTaskLoader.h>
+
+namespace mc_tasks
+{
+
+namespace force
+{
+
+DampingTask::DampingTask(const std::string & surfaceName,
+                               const mc_rbdyn::Robots & robots,
+                               unsigned int robotIndex,
+                               double stiffness,
+                               double weight)
+: AdmittanceTask(surfaceName, robots, robotIndex, stiffness, weight)
+{
+  name_ = "damping_" + robot_.name() + "_" + surfaceName;
+  reset();
+}
+
+void DampingTask::update()
+{
+  wrenchError_ = measuredWrench() - targetWrench_;
+
+  Eigen::Vector3d linearVel = admittance_.force().cwiseProduct(wrenchError_.force());
+  Eigen::Vector3d angularVel = admittance_.couple().cwiseProduct(wrenchError_.couple());
+  clampAndWarn(name_, linearVel, maxLinearVel_, "linear velocity", isClampingLinearVel_);
+  clampAndWarn(name_, angularVel, maxAngularVel_, "angular velocity", isClampingAngularVel_);
+  refVelB_ = feedforwardVelB_ + sva::MotionVecd{angularVel, linearVel};
+
+  // SC: we could do add an anti-windup strategy here, e.g. back-calculation.
+  // Yet, keep in mind that our velocity bounds are artificial. Whenever
+  // possible, the best is to set to gains so that they are not saturated.
+
+  SurfaceTransformTask::refVelB(refVelB_);
+}
+
+} // namespace force
+
+} // namespace mc_tasks
+
+namespace
+{
+
+static bool registered = mc_tasks::MetaTaskLoader::register_load_function(
+    "admittance",
+    [](mc_solver::QPSolver & solver, const mc_rtc::Configuration & config) {
+      auto t = std::make_shared<mc_tasks::force::DampingTask>(config("surface"), solver.robots(), config("robotIndex"));
+      if(config.has("admittance"))
+      {
+        t->admittance(config("admittance"));
+      }
+      if(config.has("damping"))
+      {
+        double d = config("damping");
+        t->damping(d);
+      }
+
+      if(config.has("targetSurface"))
+      {
+        const auto & c = config("targetSurface");
+        t->targetSurface(c("robotIndex"), c("surface"),
+                         {c("offset_rotation", Eigen::Matrix3d::Identity().eval()),
+                          c("offset_translation", Eigen::Vector3d::Zero().eval())});
+      }
+      else if(config.has("targetPose"))
+      {
+        t->targetPose(config("targetPose"));
+      }
+      if(config.has("weight"))
+      {
+        t->weight(config("weight"));
+      }
+      if(config.has("wrench"))
+      {
+        t->targetWrench(config("wrench"));
+      }
+      t->load(solver, config);
+      return t;
+    });
+}
+
