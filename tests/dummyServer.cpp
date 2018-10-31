31b8873237960da45d65ad220532fba282147e94
Arnaud TANGUY <arn.tanguy@gmail.com>
2018-10-31 17:21:04 +0100

    [GUI] XYTheta example

--- tests/dummyServer.cpp
+++ tests/dummyServer.cpp
@@ -33,7 +33,7 @@ struct TestServer
   sva::PTransformd rotInteractive_{Eigen::Vector3d{0., 0., 1.}};
   sva::PTransformd static_{sva::RotZ(-M_PI), Eigen::Vector3d(1., 0., 0.)};
   sva::PTransformd interactive_{Eigen::Vector3d{0., 1., 0.}};
-  Eigen::Vector3d xytheta_{1., 1., M_PI / 3};
+  Eigen::Vector3d xytheta_{0., 2., M_PI / 3};
 };
 
 TestServer::TestServer()
