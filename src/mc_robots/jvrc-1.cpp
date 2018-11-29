c6742426fcbfc62d668a6497bfcfcc50c67b2016
Pierre Gergondet <pierre.gergondet@gmail.com>
2018-11-29 14:53:45 +0900

    Add JVRC-1 RobotModule

---
+++ src/mc_robots/jvrc-1.cpp
@@ -0,0 +1,70 @@
+#ifndef JVRC_DESCRIPTION_PATH
+#  error "JVRC_DESCRIPTION_PATH must be defined to build this RobotModule"
+#endif
+
+#define JVRC_VAL(x) #x
+#define JVRC_VAL_VAL(x) JVRC_VAL(x)
+
+#include "jvrc-1.h"
+
+#include <mc_rtc/logging.h>
+
+#include <boost/filesystem.hpp>
+
+#include <fstream>
+namespace bfs = boost::filesystem;
+
+namespace mc_robots
+{
+
+JVRC1RobotModule::JVRC1RobotModule() : RobotModule(std::string(JVRC_VAL_VAL(JVRC_DESCRIPTION_PATH)), "jvrc-1")
+{
+  std::ifstream ifs(urdf_path);
+  if(ifs.is_open())
+  {
+    std::stringstream urdf;
+    urdf << ifs.rdbuf();
+    mc_rbdyn_urdf::URDFParserResult res = mc_rbdyn_urdf::rbdyn_from_urdf(urdf.str(), false);
+    mb = res.mb;
+    mbc = res.mbc;
+    mbg = res.mbg;
+    boundsFromURDF(res.limits);
+    _collisionTransforms = res.collision_tf;
+
+    std::string convexPath = path + "/convex/" + name + "/";
+    bfs::path p(convexPath);
+    if(bfs::exists(p) && bfs::is_directory(p))
+    {
+      std::vector<bfs::path> files;
+      std::copy(bfs::directory_iterator(p), bfs::directory_iterator(), std::back_inserter(files));
+      for(const bfs::path & file : files)
+      {
+        size_t off = file.filename().string().rfind("-ch.txt");
+        if(off != std::string::npos)
+        {
+          std::string name = file.filename().string();
+          name.replace(off, 7, "");
+          _convexHull[name] = std::pair<std::string, std::string>(name, file.string());
+        }
+      }
+    }
+    expand_stance();
+    make_default_ref_joint_order();
+    std::vector<double> default_q = {-0.38, -0.01, 0., 0.72, -0.01, -0.33,  -0.38, 0.02, 0.,    0.72, -0.02, -0.33, 0.,
+                                     0.13,  0.,    0., 0.,   0.,    -0.052, -0.17, 0.,   -0.52, 0.,   0.,    0.,    0.,
+                                     0.,    0.,    0., 0.,   0.,    -0.052, 0.17,  0.,   -0.52, 0.,   0.,    0.,    0.,
+                                     0.,    0.,    0., 0.,   0.,    0.,     0.,    0.,   0.,    0.,   0.};
+    const auto & rjo = ref_joint_order();
+    for(size_t i = 0; i < rjo.size(); ++i)
+    {
+      _stance[rjo[i]] = {default_q[i]};
+    }
+    _default_attitude = {{1., 0., 0., 0., 0., 0., 0.854}};
+  }
+  else
+  {
+    LOG_ERROR_AND_THROW(std::runtime_error, "Could not load JVRC-1 model at " << urdf_path)
+  }
+}
+
+} // namespace mc_robots
---
+++ src/mc_robots/jvrc-1.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <mc_rbdyn/RobotModule.h>
+
+#include <mc_rbdyn_urdf/urdf.h>
+
+#include "api.h"
+
+namespace mc_robots
+{
+
+struct MC_ROBOTS_DLLAPI JVRC1RobotModule : public mc_rbdyn::RobotModule
+{
+public:
+  JVRC1RobotModule();
+};
+
+} // namespace mc_robots
+
+extern "C"
+{
+  ROBOT_MODULE_API void MC_RTC_ROBOT_MODULE(std::vector<std::string> & names)
+  {
+    names = {"JVRC-1"};
+  }
+  ROBOT_MODULE_API void destroy(mc_rbdyn::RobotModule * ptr)
+  {
+    delete ptr;
+  }
+  ROBOT_MODULE_API mc_rbdyn::RobotModule * create(const std::string &)
+  {
+    return new mc_robots::JVRC1RobotModule();
+  }
+}
