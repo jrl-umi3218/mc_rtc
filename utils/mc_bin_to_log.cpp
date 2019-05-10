012d5fbe51aada6be5edccdb6b212fb7acad22ac
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-05-10 11:53:11 +0900

    [mc_bin_to_log] Use FlatLog to perform the conversion

--- utils/mc_bin_to_log.cpp
+++ utils/mc_bin_to_log.cpp
@@ -2,259 +2,338 @@
  * Copyright 2015-2019 CNRS-UM LIRMM, CNRS-AIST JRL
  */
 
-#include <mc_rtc/log/serialization/fb_csv_utils.h>
-#include <mc_rtc/logging.h>
-
+#include "mc_bin_utils.h"
 #include <fstream>
 
-typedef std::tuple<std::string, mc_rtc::log::LogData, size_t> typed_key;
-
-struct LogEntry
+struct SizedType
 {
-  const mc_rtc::log::Log * entry;
-  char * raw;
-  std::vector<typed_key> keys()
-  {
-    std::vector<typed_key> ret;
-    if(entry->keys())
-    {
-      const auto & keys = *entry->keys();
-      const auto & values_type = *entry->values_type();
-      const auto & values = *entry->values();
-      for(flatbuffers::uoffset_t i = 0; i < static_cast<flatbuffers::uoffset_t>(keys.size()); ++i)
-      {
-        auto k = keys[i];
-        auto vt = mc_rtc::log::LogData(values_type[i]);
-        size_t size = 0;
-#define CASE_ENUM(VALUE)                                             \
-  case VALUE:                                                        \
-    size = mc_rtc::log::CSVWriterHelper<VALUE>::key_size(values[i]); \
-    break;
-        switch(vt)
-        {
-          CASE_ENUM(mc_rtc::log::LogData_Bool)
-          CASE_ENUM(mc_rtc::log::LogData_Double)
-          CASE_ENUM(mc_rtc::log::LogData_DoubleVector)
-          CASE_ENUM(mc_rtc::log::LogData_UnsignedInt)
-          CASE_ENUM(mc_rtc::log::LogData_UInt64)
-          CASE_ENUM(mc_rtc::log::LogData_String)
-          CASE_ENUM(mc_rtc::log::LogData_Vector2d)
-          CASE_ENUM(mc_rtc::log::LogData_Vector3d)
-          CASE_ENUM(mc_rtc::log::LogData_Quaterniond)
-          CASE_ENUM(mc_rtc::log::LogData_PTransformd)
-          CASE_ENUM(mc_rtc::log::LogData_ForceVecd)
-          CASE_ENUM(mc_rtc::log::LogData_MotionVecd)
-          default:
-            break;
-        };
-#undef CASE_ENUM
-        ret.push_back(std::make_tuple(k->str(), vt, size));
-      }
-    }
-    return ret;
-  }
+  mc_rtc::log::LogType type;
+  size_t size;
 };
 
-struct Log
+using SizedEntries = std::map<std::string, SizedType>;
+
+SizedEntries write_header(std::ofstream & ofs, const mc_rtc::log::FlatLog & log)
 {
-  Log(const std::string & name)
+  SizedEntries out;
+  auto entries = utils::entries(log);
+  size_t i = 0;
+  for(const auto & e : entries)
   {
-    std::ifstream ifs(name, std::ifstream::binary);
-    if(!ifs.is_open())
+    auto s = utils::entrySize(log, e.first, e.second);
+    // clang-format off
+    switch(e.second)
     {
-      LOG_ERROR(name << " could not be opened")
-      std::exit(1);
-    }
-    while(ifs)
-    {
-      int size = 0;
-      ifs.read((char *)(&size), sizeof(int));
-      if(ifs)
-      {
-        char * data = new char[size];
-        ifs.read(data, size);
-        entries.push_back({mc_rtc::log::GetLog(data), data});
-        auto new_keys = entries.back().keys();
-        if(keys.size() == 0)
+      case mc_rtc::log::LogType::Bool:
+      case mc_rtc::log::LogType::Int8_t:
+      case mc_rtc::log::LogType::Int16_t:
+      case mc_rtc::log::LogType::Int32_t:
+      case mc_rtc::log::LogType::Int64_t:
+      case mc_rtc::log::LogType::Uint8_t:
+      case mc_rtc::log::LogType::Uint16_t:
+      case mc_rtc::log::LogType::Uint32_t:
+      case mc_rtc::log::LogType::Uint64_t:
+      case mc_rtc::log::LogType::Float:
+      case mc_rtc::log::LogType::Double:
+      case mc_rtc::log::LogType::String:
+        ofs << e.first;
+        break;
+      case mc_rtc::log::LogType::Quaterniond:
+        ofs << e.first << "_w;"
+            << e.first << "_x;"
+            << e.first << "_y;"
+            << e.first << "_z";
+        break;
+      case mc_rtc::log::LogType::Vector2d:
+        ofs << e.first << "_x;"
+            << e.first << "_y";
+        break;
+      case mc_rtc::log::LogType::Vector3d:
+        ofs << e.first << "_x;"
+            << e.first << "_y;"
+            << e.first << "_z";
+        break;
+      case mc_rtc::log::LogType::Vector6d:
+        ofs << e.first << "_0;"
+            << e.first << "_1;"
+            << e.first << "_2;"
+            << e.first << "_3;"
+            << e.first << "_4;"
+            << e.first << "_5";
+        break;
+      case mc_rtc::log::LogType::PTransformd:
+        ofs << e.first << "_qw;"
+            << e.first << "_qx;"
+            << e.first << "_qy;"
+            << e.first << "_qz;"
+            << e.first << "_tx;"
+            << e.first << "_ty;"
+            << e.first << "_tz";
+        break;
+      case mc_rtc::log::LogType::ForceVecd:
+        ofs << e.first << "_cx;"
+            << e.first << "_cy;"
+            << e.first << "_cz;"
+            << e.first << "_fx;"
+            << e.first << "_fy;"
+            << e.first << "_fz";
+        break;
+      case mc_rtc::log::LogType::MotionVecd:
+        ofs << e.first << "_wx;"
+            << e.first << "_wy;"
+            << e.first << "_wz;"
+            << e.first << "_vx;"
+            << e.first << "_vy;"
+            << e.first << "_vz";
+        break;
+      case mc_rtc::log::LogType::VectorXd:
+      case mc_rtc::log::LogType::VectorDouble:
+        for(size_t j = 0; j < s; ++j)
         {
-          keys = new_keys;
-        }
-        else
-        {
-          for(const auto & nk : new_keys)
+          ofs << e.first << "_" << j;
+          if(j != s - 1)
           {
-            const std::string & k = std::get<0>(nk);
-            auto it = std::find_if(keys.begin(), keys.end(),
-                                   [&k](std::vector<typed_key>::value_type & kit) { return std::get<0>(kit) == k; });
-            if(it != keys.end())
-            {
-              size_t n_size = std::get<2>(nk);
-              size_t size = std::get<2>(*it);
-              if(n_size != size)
-              {
-                LOG_ERROR("Key " << k << " was first seen with size " << size << " but was found again with size "
-                                 << n_size)
-                LOG_ERROR("The resulting CSV will be malformed")
-              }
-            }
-            else
-            {
-              keys.push_back(nk);
-            }
+            ofs << ';';
           }
         }
-      }
-    }
-  }
-
-  void write_header(std::ostream & os)
-  {
-    std::string buffer;
-    bool start = true;
-    for(const auto & k : keys)
-    {
-      const auto & key = std::get<0>(k);
-      auto vt = std::get<1>(k);
-      size_t size = std::get<2>(k);
-      if(size == 0)
-      {
+        break;
+      case mc_rtc::log::LogType::None:
+        LOG_ERROR(e.first << " cannot be written into log format")
         continue;
-      }
-      if(start)
-      {
-        start = false;
-      }
-      else
-      {
-        buffer += ";";
-      }
-#define CASE_ENUM(VALUE)                                                  \
-  case VALUE:                                                             \
-    mc_rtc::log::CSVWriterHelper<VALUE>::write_header(key, size, buffer); \
-    break;
-      switch(vt)
-      {
-        CASE_ENUM(mc_rtc::log::LogData_Bool)
-        CASE_ENUM(mc_rtc::log::LogData_Double)
-        CASE_ENUM(mc_rtc::log::LogData_DoubleVector)
-        CASE_ENUM(mc_rtc::log::LogData_UnsignedInt)
-        CASE_ENUM(mc_rtc::log::LogData_UInt64)
-        CASE_ENUM(mc_rtc::log::LogData_String)
-        CASE_ENUM(mc_rtc::log::LogData_Vector2d)
-        CASE_ENUM(mc_rtc::log::LogData_Vector3d)
-        CASE_ENUM(mc_rtc::log::LogData_Quaterniond)
-        CASE_ENUM(mc_rtc::log::LogData_PTransformd)
-        CASE_ENUM(mc_rtc::log::LogData_ForceVecd)
-        CASE_ENUM(mc_rtc::log::LogData_MotionVecd)
-        default:
-          break;
-      };
-#undef CASE_ENUM
     }
-    buffer += '\n';
-    os << buffer;
+    // clang-format on
+    if(++i != entries.size())
+    {
+      ofs << ';';
+    }
+    else
+    {
+      ofs << '\n';
+    }
+    out[e.first] = {e.second, s};
   }
+  return out;
+}
 
-  void write_entry(LogEntry & entry, std::ostream & os, std::vector<typed_key> & current_keys)
+// clang-format off
+template<typename T>
+void write_data(std::ofstream & ofs, const T & data, size_t /*fsize*/)
+{
+  ofs << data;
+}
+
+template<>
+void write_data<Eigen::Quaterniond>(std::ofstream & ofs, const Eigen::Quaterniond & data, size_t /*fsize*/)
+{
+  ofs << data.w() << ";"
+      << data.x() << ";"
+      << data.y() << ";"
+      << data.z();
+}
+
+template<>
+void write_data<Eigen::Vector2d>(std::ofstream & ofs, const Eigen::Vector2d & data, size_t /*fsize*/)
+{
+  ofs << data.x() << ";"
+      << data.y();
+}
+
+template<>
+void write_data<Eigen::Vector3d>(std::ofstream & ofs, const Eigen::Vector3d & data, size_t /*fsize*/)
+{
+  ofs << data.x() << ";"
+      << data.y() << ";"
+      << data.z();
+}
+
+template<>
+void write_data<Eigen::Vector6d>(std::ofstream & ofs, const Eigen::Vector6d & data, size_t /*fsize*/)
+{
+  ofs << data(0) << ";"
+      << data(1) << ";"
+      << data(2) << ";"
+      << data(3) << ";"
+      << data(4) << ";"
+      << data(5);
+}
+// clang-format on
+
+template<>
+void write_data<Eigen::VectorXd>(std::ofstream & ofs, const Eigen::VectorXd & data, size_t fsize)
+{
+  for(int i = 0; i < data.size(); ++i)
   {
-    std::string buffer;
-    auto entry_keys = entry.keys();
-    if(entry_keys.size())
+    ofs << data(i);
+    if(i + 1 != static_cast<int>(fsize))
     {
-      current_keys = entry_keys;
+      ofs << ';';
     }
-    bool start = true;
-    const auto & values = *entry.entry->values();
-    auto get_key_index = [&current_keys](const std::string & k) {
-      for(size_t i = 0; i < current_keys.size(); ++i)
-      {
-        const std::string & kn = std::get<0>(current_keys[i]);
-        if(kn == k)
-        {
-          return i;
-        }
-      }
-      return current_keys.size();
-    };
-    for(const auto & k : keys)
+  }
+  for(int i = data.size(); i < static_cast<int>(fsize); ++i)
+  {
+    if(i + 1 != static_cast<int>(fsize))
     {
-      const std::string & key = std::get<0>(k);
-      auto vt = std::get<1>(k);
-      size_t size = std::get<2>(k);
-      if(size == 0)
-      {
+      ofs << ';';
+    }
+  }
+}
+
+template<>
+void write_data<std::vector<double>>(std::ofstream & ofs, const std::vector<double> & data, size_t fsize)
+{
+  for(size_t i = 0; i < data.size(); ++i)
+  {
+    ofs << data[i];
+    if(i + 1 != fsize)
+    {
+      ofs << ';';
+    }
+  }
+  for(size_t i = data.size(); i < fsize; ++i)
+  {
+    if(i + 1 != fsize)
+    {
+      ofs << ';';
+    }
+  }
+}
+
+template<>
+void write_data<sva::PTransformd>(std::ofstream & ofs, const sva::PTransformd & data, size_t s)
+{
+  write_data(ofs, Eigen::Quaterniond(data.rotation()), s);
+  ofs << ';';
+  write_data(ofs, data.translation(), s);
+}
+
+template<>
+void write_data<sva::ForceVecd>(std::ofstream & ofs, const sva::ForceVecd & data, size_t s)
+{
+  write_data(ofs, data.vector(), s);
+}
+
+template<>
+void write_data<sva::MotionVecd>(std::ofstream & ofs, const sva::MotionVecd & data, size_t s)
+{
+  write_data(ofs, data.vector(), s);
+}
+
+template<typename T>
+void write_data(std::ofstream & ofs,
+                const mc_rtc::log::FlatLog & log,
+                const std::string & entry,
+                size_t idx,
+                size_t fsize)
+{
+  const T * data = log.getRaw<T>(entry, idx);
+  if(data)
+  {
+    write_data<T>(ofs, *data, fsize);
+  }
+  else
+  {
+    for(size_t i = 0; i < fsize - 1; ++i)
+    {
+      ofs << ';';
+    }
+  }
+}
+
+void write_data(std::ofstream & ofs, const mc_rtc::log::FlatLog & log, const SizedEntries & entries, size_t idx)
+{
+  size_t i = 0;
+  for(const auto & e : entries)
+  {
+    switch(e.second.type)
+    {
+      case mc_rtc::log::LogType::Bool:
+        write_data<bool>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Int8_t:
+        write_data<int8_t>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Int16_t:
+        write_data<int16_t>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Int32_t:
+        write_data<int32_t>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Int64_t:
+        write_data<int64_t>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Uint8_t:
+        write_data<uint8_t>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Uint16_t:
+        write_data<uint16_t>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Uint32_t:
+        write_data<uint32_t>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Uint64_t:
+        write_data<uint64_t>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Float:
+        write_data<float>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Double:
+        write_data<double>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::String:
+        write_data<std::string>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Quaterniond:
+        write_data<Eigen::Quaterniond>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Vector2d:
+        write_data<Eigen::Vector2d>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Vector3d:
+        write_data<Eigen::Vector3d>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::Vector6d:
+        write_data<Eigen::Vector6d>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::VectorXd:
+        write_data<Eigen::VectorXd>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::PTransformd:
+        write_data<sva::PTransformd>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::ForceVecd:
+        write_data<sva::ForceVecd>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::MotionVecd:
+        write_data<sva::MotionVecd>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::VectorDouble:
+        write_data<std::vector<double>>(ofs, log, e.first, idx, e.second.size);
+        break;
+      case mc_rtc::log::LogType::None:
         continue;
-      }
-      if(start)
-      {
-        start = false;
-      }
-      else
-      {
-        buffer += ";";
-      }
-      size_t idx = get_key_index(key);
-      if(idx != current_keys.size())
-      {
-        const void * value = values[static_cast<flatbuffers::uoffset_t>(idx)];
-#define CASE_ENUM(VALUE)                                            \
-  case VALUE:                                                       \
-    mc_rtc::log::CSVWriterHelper<VALUE>::write_data(value, buffer); \
-    break;
-        switch(vt)
-        {
-          CASE_ENUM(mc_rtc::log::LogData_Bool)
-          CASE_ENUM(mc_rtc::log::LogData_Double)
-          CASE_ENUM(mc_rtc::log::LogData_DoubleVector)
-          CASE_ENUM(mc_rtc::log::LogData_UnsignedInt)
-          CASE_ENUM(mc_rtc::log::LogData_UInt64)
-          CASE_ENUM(mc_rtc::log::LogData_String)
-          CASE_ENUM(mc_rtc::log::LogData_Vector2d)
-          CASE_ENUM(mc_rtc::log::LogData_Vector3d)
-          CASE_ENUM(mc_rtc::log::LogData_Quaterniond)
-          CASE_ENUM(mc_rtc::log::LogData_PTransformd)
-          CASE_ENUM(mc_rtc::log::LogData_ForceVecd)
-          CASE_ENUM(mc_rtc::log::LogData_MotionVecd)
-          default:
-            break;
-        };
-#undef CASE_ENUM
-      }
-      else
-      {
-        for(size_t i = 1; i < size; ++i)
-        {
-          buffer += ";";
-        }
-      }
     }
-    buffer += '\n';
-    os << buffer;
-  }
-
-  void convert(const std::string & out)
-  {
-    std::ofstream ofs(out);
-    write_header(ofs);
-    std::vector<typed_key> current_keys = {};
-    for(auto & e : entries)
+    if(++i != entries.size())
     {
-      write_entry(e, ofs, current_keys);
+      ofs << ';';
     }
-  }
-  ~Log()
-  {
-    for(auto & e : entries)
+    else
     {
-      delete[] e.raw;
+      ofs << '\n';
     }
   }
-  std::vector<LogEntry> entries;
-  std::vector<typed_key> keys;
-};
+}
 
 void mc_bin_to_log(const std::string & in, const std::string & out)
 {
-  Log log(in);
-  log.convert(out);
+  mc_rtc::log::FlatLog log(in);
+  std::ofstream ofs(out);
+  if(!ofs.is_open())
+  {
+    LOG_ERROR_AND_THROW(std::runtime_error, "Failed to open " << out << " for conversion from bin to log");
+  }
+  auto entries = write_header(ofs, log);
+  for(size_t i = 0; i < log.size(); ++i)
+  {
+    write_data(ofs, log, entries, i);
+  }
 }
