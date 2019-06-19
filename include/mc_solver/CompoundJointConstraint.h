81cc42eed588ed4b52890ba78d97253ab7a91429
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-06-19 18:45:43 +0900

    [mc_solver/mc_rbdyn] Implement CompoundJointConstraint
    Also let RobotModule describe compound constraints

---
+++ include/mc_solver/CompoundJointConstraint.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <mc_rbdyn/CompoundJointConstraintDescription.h>
+#include <mc_rbdyn/Robots.h>
+#include <mc_solver/ConstraintSet.h>
+
+#include <Tasks/QPSolver.h>
+
+namespace mc_solver
+{
+
+using CompoundJointConstraintDescription = mc_rbdyn::CompoundJointConstraintDescription;
+
+namespace details
+{
+
+/** Implement compound joint constraints
+ *
+ * This constraint is given a set of CompoundJointConstraint for a given robot
+ * and will enforce them
+ */
+struct MC_SOLVER_DLLAPI CompoundJointConstraint : public tasks::qp::ConstraintFunction<tasks::qp::Inequality>
+{
+  CompoundJointConstraint(const mc_rbdyn::Robots & robots,
+                          unsigned int rIndex,
+                          double dt,
+                          const std::vector<CompoundJointConstraintDescription> & desc = {});
+
+  ~CompoundJointConstraint() override;
+
+  void addConstraint(const mc_rbdyn::Robots & robots,
+                     unsigned int rIndex,
+                     const CompoundJointConstraintDescription & desc);
+
+  void updateNrVars(const std::vector<rbd::MultiBody> & mbs, const tasks::qp::SolverData & data) override;
+
+  void update(const std::vector<rbd::MultiBody> & mbs,
+              const std::vector<rbd::MultiBodyConfig> & mbcs,
+              const tasks::qp::SolverData & data) override;
+
+  inline const Eigen::MatrixXd & AInEq() const override
+  {
+    return A_;
+  }
+
+  inline int maxInEq() const override
+  {
+    return descs_.size();
+  }
+
+  inline std::string nameInEq() const override
+  {
+    return name_;
+  }
+
+  inline const Eigen::VectorXd & bInEq() const override
+  {
+    return b_;
+  }
+
+  std::string descInEq(const std::vector<rbd::MultiBody> & mbs, int i) override;
+
+private:
+  size_t rIndex_;
+  std::string name_;
+  Eigen::MatrixXd A_;
+  Eigen::VectorXd b_;
+  // Constant part of b_
+  Eigen::VectorXd b_cst_;
+  // Timestep
+  double dt_;
+  // Simplified form of the description
+  struct Desc
+  {
+    // Index of j1 in mbc
+    size_t q1Idx;
+    // Index of j1 in matrix
+    int q1MatIdx;
+    // Index of j2 in mbc
+    size_t q2Idx;
+    // Index of j2 in matrix
+    int q2MatIdx;
+    // x index of p1
+    double p1_x;
+    // y index of p1
+    double p1_y;
+    // P_x = p2_x - p1_x
+    double P_x;
+    // P_y = p2_y - p1_y
+    double P_y;
+  };
+  std::vector<Desc> descs_;
+};
+
+} // namespace details
+
+/** Wrapper for mc_solver::details::CompundJointConstraint */
+struct MC_SOLVER_DLLAPI CompoundJointConstraint : public ConstraintSet
+{
+  CompoundJointConstraint(const mc_rbdyn::Robots & robots, unsigned int robotIndex, double dt);
+
+  void addToSolver(const std::vector<rbd::MultiBody> & mbs, tasks::qp::QPSolver & solver) override;
+
+  void removeFromSolver(tasks::qp::QPSolver & solver) override;
+
+private:
+  details::CompoundJointConstraint constr_;
+};
+
+} // namespace mc_solver
---
+++ src/mc_solver/CompoundJointConstraint.cpp
@@ -0,0 +1,117 @@
+#include <mc_rtc/logging.h>
+#include <mc_solver/CompoundJointConstraint.h>
+#include <mc_solver/ConstraintSetLoader.h>
+
+namespace mc_solver
+{
+
+namespace details
+{
+
+CompoundJointConstraint::CompoundJointConstraint(const mc_rbdyn::Robots & robots,
+                                                 unsigned int rIndex,
+                                                 double dt,
+                                                 const std::vector<CompoundJointConstraintDescription> & desc)
+: rIndex_(rIndex), name_("CompoundJointConstraint_" + robots.robot(rIndex).name()), dt_(dt)
+{
+  for(const auto & d : desc)
+  {
+    addConstraint(robots, rIndex, d);
+  }
+}
+
+CompoundJointConstraint::~CompoundJointConstraint() {}
+
+void CompoundJointConstraint::addConstraint(const mc_rbdyn::Robots & robots,
+                                            unsigned int rIndex,
+                                            const CompoundJointConstraintDescription & desc)
+{
+  const auto & robot = robots.robot(rIndex);
+  if(rIndex != rIndex_)
+  {
+    LOG_ERROR_AND_THROW(std::runtime_error, "You must create one CompoundJointConstraint per robot");
+  }
+  auto check_joint = [&](const std::string & jname) {
+    if(!robot.hasJoint(jname))
+    {
+      LOG_ERROR_AND_THROW(std::runtime_error, "No joint named " << jname << " in " << robot.name())
+    }
+    auto qIdx = robot.jointIndexByName(jname);
+    if(robot.mb().joint(qIdx).dof() != 1)
+    {
+      LOG_ERROR_AND_THROW(std::runtime_error, "Joint " << jname << " does not have exactly one dof")
+    }
+    return qIdx;
+  };
+  auto q1Idx = check_joint(desc.j1);
+  auto q2Idx = check_joint(desc.j2);
+  descs_.push_back({q1Idx, robot.mb().jointPosInDof(q1Idx), q2Idx, robot.mb().jointPosInDof(q2Idx), desc.p1.x(),
+                    desc.p1.y(), desc.p2.x() - desc.p1.x(), desc.p2.y() - desc.p1.y()});
+}
+
+void CompoundJointConstraint::updateNrVars(const std::vector<rbd::MultiBody> &, const tasks::qp::SolverData & data)
+{
+  A_.setZero(descs_.size(), data.nrVars());
+  b_.setZero(descs_.size());
+  b_cst_.setZero(descs_.size());
+  auto ABegin = data.alphaDBegin(rIndex_);
+  for(size_t i = 0; i < descs_.size(); ++i)
+  {
+    const auto & d = descs_[i];
+    A_(i, ABegin + d.q1MatIdx) = dt_ * dt_ * d.P_y / 2;
+    A_(i, ABegin + d.q2MatIdx) = -dt_ * dt_ * d.P_x / 2;
+    b_cst_(i) = d.p1_x * d.P_y - d.p1_y * d.P_x;
+  }
+}
+
+void CompoundJointConstraint::update(const std::vector<rbd::MultiBody> &,
+                                     const std::vector<rbd::MultiBodyConfig> & mbcs,
+                                     const tasks::qp::SolverData &)
+{
+  for(size_t i = 0; i < descs_.size(); ++i)
+  {
+    const auto & d = descs_[i];
+    const auto & q1 = mbcs[rIndex_].q[d.q1Idx][0];
+    const auto & alpha1 = mbcs[rIndex_].alpha[d.q1Idx][0];
+    const auto & q2 = mbcs[rIndex_].q[d.q2Idx][0];
+    const auto & alpha2 = mbcs[rIndex_].alpha[d.q2Idx][0];
+    b_(i) = b_cst_(i) + d.P_x * q2 - d.P_y * q1 + dt_ * (d.P_x * alpha2 - d.P_y * alpha1);
+  }
+}
+
+std::string CompoundJointConstraint::descInEq(const std::vector<rbd::MultiBody> &, int i)
+{
+  std::stringstream ss;
+  ss << "Error in " << name_ << " at line " << i << "\n";
+  return ss.str();
+}
+
+} // namespace details
+
+CompoundJointConstraint::CompoundJointConstraint(const mc_rbdyn::Robots & robots, unsigned int rIndex, double dt)
+: constr_(robots, rIndex, dt, robots.robot(rIndex).module().compoundJoints())
+{
+}
+
+void CompoundJointConstraint::addToSolver(const std::vector<rbd::MultiBody> & mbs, tasks::qp::QPSolver & solver)
+{
+  constr_.addToSolver(mbs, solver);
+  solver.updateConstrSize();
+}
+
+void CompoundJointConstraint::removeFromSolver(tasks::qp::QPSolver & solver)
+{
+  constr_.removeFromSolver(solver);
+}
+
+} // namespace mc_solver
+
+namespace
+{
+
+static bool registered = mc_solver::ConstraintSetLoader::register_load_function(
+    "compoundJoint",
+    [](mc_solver::QPSolver & solver, const mc_rtc::Configuration & config) {
+      return std::make_shared<mc_solver::CompoundJointConstraint>(solver.robots(), config("robotIndex"), solver.dt());
+    });
+}
