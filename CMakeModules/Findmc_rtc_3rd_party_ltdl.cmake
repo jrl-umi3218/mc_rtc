55ff5f9702812368aff8c83025b12b295cb9ccf6
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-06-19 18:43:34 +0900

    [cmake] Make sur 3rd-party modules can be included multiple times

--- CMakeModules/Findmc_rtc_3rd_party_ltdl.cmake
+++ CMakeModules/Findmc_rtc_3rd_party_ltdl.cmake
@@ -8,27 +8,31 @@
 #
 # Defines the mc_rtc_3rd_party::ltdl target
 
-if(NOT DEFINED LTDL_PREFIX)
-  set(LTDL_PREFIX ${CMAKE_INSTALL_PREFIX})
-endif()
-
-find_path(LTDL_INCLUDE_DIR
-  NAMES ltdl.h
-  HINTS ${LTDL_PREFIX}
-  )
-find_library(LTDL_LIBRARY
-  NAMES ltdl
-  PATHS ${LTDL_PREFIX}
-  )
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LTDL DEFAULT_MSG LTDL_LIBRARY LTDL_INCLUDE_DIR)
-mark_as_advanced(LTDL_INCLUDE_DIR LTDL_LIBRARY)
 set(LTDL mc_rtc_3rd_party::ltdl)
-if(LTDL_FOUND)
-  add_library(${LTDL} INTERFACE IMPORTED GLOBAL)
-  set_target_properties(${LTDL} PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${LTDL_INCLUDE_DIR}
-    INTERFACE_LINK_LIBRARIES ${LTDL_LIBRARY}
+if(NOT TARGET ${LTDL})
+
+  if(NOT DEFINED LTDL_PREFIX)
+    set(LTDL_PREFIX ${CMAKE_INSTALL_PREFIX})
+  endif()
+  
+  find_path(LTDL_INCLUDE_DIR
+    NAMES ltdl.h
+    HINTS ${LTDL_PREFIX}
     )
+  find_library(LTDL_LIBRARY
+    NAMES ltdl
+    PATHS ${LTDL_PREFIX}
+    )
+  
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(LTDL DEFAULT_MSG LTDL_LIBRARY LTDL_INCLUDE_DIR)
+  mark_as_advanced(LTDL_INCLUDE_DIR LTDL_LIBRARY)
+  if(LTDL_FOUND)
+    add_library(${LTDL} INTERFACE IMPORTED GLOBAL)
+    set_target_properties(${LTDL} PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES ${LTDL_INCLUDE_DIR}
+      INTERFACE_LINK_LIBRARIES ${LTDL_LIBRARY}
+      )
+  endif()
+
 endif()
