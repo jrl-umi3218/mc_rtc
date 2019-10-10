02822f609442df020b7e851a8f15e2fd2b1618f5
Arnaud TANGUY <arn.tanguy@gmail.com>
2019-10-10 23:55:19 +0200

    [Trajectory][cmake] Fix Find hpp-spline cmake

--- CMakeModules/Findmc_rtc_3rd_party_hpp-spline.cmake
+++ CMakeModules/Findmc_rtc_3rd_party_hpp-spline.cmake
@@ -7,17 +7,28 @@
 # Finds the hpp-spline library
 # https://github.com/humanoid-path-planner/hpp-spline
 #
-# This will define the following imported targets
+# The following variables will be set
+#
+# hpp-spline_FOUND False
+#
+# If hpp-spline is found the following imported targets will be defined
 #
 #     mc_rtc_3rd_party::hpp-spline
 
 find_package(PkgConfig)
 pkg_check_modules(hpp-spline QUIET hpp-spline)
 
-if(hpp-spline_FOUND AND NOT TARGET hpp-spline::hpp-spline)
-    add_library(mc_rtc_3rd_party::hpp-spline INTERFACE IMPORTED)
-    set_target_properties(mc_rtc_3rd_party::hpp-spline PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${hpp-spline_INCLUDE_DIR}"
-    )
+if(${hpp-spline_FOUND})
+  set(hpp-spline_FOUND True)
+  if(hpp-spline_FOUND AND NOT TARGET hpp-spline::hpp-spline)
+      add_library(mc_rtc_3rd_party::hpp-spline INTERFACE IMPORTED)
+      set_target_properties(mc_rtc_3rd_party::hpp-spline PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${hpp-spline_INCLUDE_DIRS}"
+      )
+  endif()
+  message("-- Found hpp-spline include directories: ${hpp-spline_INCLUDE_DIRS}")
+else()
+  set(hpp-spline_FOUND False)
 endif()
+
 
