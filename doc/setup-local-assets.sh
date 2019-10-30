a03463b6b08320e67c9f02613d7c726497256a9d
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-10-30 10:21:31 +0800

    [doc] Support site.baseurl and add clipboard js

--- doc/setup-local-assets.sh
+++ doc/setup-local-assets.sh
@@ -10,3 +10,4 @@ cd $doc_dir/assets/js
 wget -O jquery.min.js    https://code.jquery.com/jquery-3.3.1.slim.min.js
 wget -O popper.min.js    https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js
 wget -O bootstrap.min.js https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js
+wget -O clipboard.min.js https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js
