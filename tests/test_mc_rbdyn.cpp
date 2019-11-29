1f87150345ebdd7d84b7e8a0c4b18a6e264797fa
Arnaud TANGUY <arnaud.tanguy@lirmm.fr>
2019-11-29 16:28:50 +0900

    [bugfix][tests] Fix Robot::velW implementation
    The current implementation of Robot::velW setter is wrong:
    - It takes as input a world velocity
    - It directly sets mbc().alpha from it, which is a body velocity
    
    This commit:
    - fixes the implementation by converted the world velocity to body velocity before assigning it to mbc().alpha.
    - adds regression tests to ensure future corectness

---
+++ tests/test_mc_rbdyn.cpp
@@ -0,0 +1,48 @@
+#include <mc_rbdyn/RobotLoader.h>
+#include <mc_rbdyn/Robots.h>
+#include <mc_rbdyn/rpy_utils.h>
+#include <boost/test/unit_test.hpp>
+#include "utils.h"
+
+mc_rbdyn::Robots & get_robots()
+{
+  static std::shared_ptr<mc_rbdyn::Robots> robots_ptr = nullptr;
+  if(robots_ptr)
+  {
+    return *robots_ptr;
+  }
+  configureRobotLoader();
+  auto rm = mc_rbdyn::RobotLoader::get_robot_module("JVRC1");
+  auto env = mc_rbdyn::RobotLoader::get_robot_module("env", std::string(mc_rtc::MC_ENV_DESCRIPTION_PATH),
+                                                     std::string("ground"));
+  robots_ptr = mc_rbdyn::loadRobots({rm, env});
+  return *robots_ptr;
+}
+
+BOOST_AUTO_TEST_CASE(TestRobotPosWVelW)
+{
+  auto robots = get_robots();
+
+  for(int i = 0; i < 100; ++i)
+  {
+    Eigen::Vector3d oriRPYRef = Eigen::Vector3d::Random();
+    Eigen::Vector3d posRPYRef = Eigen::Vector3d::Random();
+    sva::PTransformd refPosW(mc_rbdyn::rpyToMat(oriRPYRef), posRPYRef);
+    robots.robot().posW(refPosW);
+    BOOST_CHECK(robots.robot().posW().matrix().isApprox(refPosW.matrix()));
+  }
+
+  for(int i = 0; i < 100; ++i)
+  {
+    auto refVal = sva::MotionVecd{Eigen::Vector3d::Random(), Eigen::Vector3d::Random()};
+    robots.robot().velW(refVal);
+    auto actual = robots.robot().velW();
+    BOOST_CHECK_MESSAGE(actual.vector().isApprox(refVal.vector()), "Error in Robot::velW"
+                                                                       << "\nExpected:"
+                                                                       << "\nangular:" << refVal.angular().transpose()
+                                                                       << "\nlinear :" << refVal.linear().transpose()
+                                                                       << "\nGot:"
+                                                                       << "\nangular:" << actual.angular().transpose()
+                                                                       << "\nlinear :" << actual.linear().transpose());
+  }
+}
