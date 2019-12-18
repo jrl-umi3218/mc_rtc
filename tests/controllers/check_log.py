3c2c8fb4d6d6ca155df6d429b986bf0230ae6921
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-12-18 17:10:08 +0800

    [tests] Run logging tests on all platforms

--- tests/controllers/check_log.py
+++ tests/controllers/check_log.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python2
-
 #
 # Copyright 2015-2019 CNRS-UM LIRMM, CNRS-AIST JRL
 #
 
+import glob
 import os
 import subprocess
 import sys
@@ -14,15 +13,19 @@ if __name__ == "__main__":
     nrIter = int(sys.argv[2])
     utils_dir = sys.argv[3]
     assert(nrIter > 0)
-    bin_path = "/tmp/mc-rtc-test-{0}-latest.bin".format(ctl)
+    if os.name == "nt":
+        bin_path = max(glob.glob(os.environ['TEMP'] + "/mc-rtc-test-{0}-*.bin".format(ctl)), key = os.path.getctime)
+    else:
+        bin_path = "/tmp/mc-rtc-test-{0}-latest.bin".format(ctl)
     assert(os.path.exists(bin_path))
     log_path = bin_path.replace('.bin', '.log')
     subprocess.call([utils_dir + "/mc_bin_to_log", bin_path, log_path])
     assert(os.path.exists(log_path))
     with open(log_path) as fd:
         lines = fd.readlines()
+        print("len(lines) is {}".format(len(lines)))
         assert(len(lines) == nrIter + 1)
         t_col = lines[0].split(';').index('t')
-        t = [ int(round(float(lines[i].split(';')[t_col])/0.005)) for i in xrange(1, nrIter + 1) ]
-        assert(t == range(nrIter))
+        t = [ int(round(float(lines[i].split(';')[t_col])/0.005)) for i in range(1, nrIter + 1) ]
+        assert(t == list(range(nrIter)))
     sys.exit(0)
