jrl-umi3218/mc_rtc

commit acbb1fccf1978bedb5755b84552e3008e8bba991

[global] Look for configuration files in controller module paths
- Simpler to manage config for controllers loaded outside the cannonic
install path (e.g. catkin build and test)
- Doesn't affect default behaviour

src/mc_control/mc_global_controller_configuration.cpp
@@ -402,7 +402,7 @@ bfs::path conf_or_yaml(bfs::path in)
 /** Load configurations */
 void load_configs(const std::string & desc,
                   const std::vector<std::string> & names,
-                  const bfs::path & default_path,
+                  const std::vector<std::string> & search_path,
                   const bfs::path & user_path,
                   std::unordered_map<std::string, mc_rtc::Configuration> & configs,
                   const mc_rtc::Configuration & default_config = {})
@@ -411,11 +411,14 @@ void load_configs(const std::string & desc,
   {
     mc_rtc::Configuration c;
     c.load(default_config);
-    bfs::path global = conf_or_yaml(default_path / (name + ".conf"));
-    if(bfs::exists(global))
+    for(const auto & p : search_path)
     {
-      LOG_INFO("Loading additional " << desc << " configuration: " << global)
-      c.load(global.string());
+      bfs::path global = bfs::path(p) / "etc" / (name + ".conf");
+      if(bfs::exists(global))
+      {
+        LOG_INFO("Loading additional " << desc << " configuration: " << global)
+        c.load(global.string());
+      }
     }
     bfs::path local = conf_or_yaml(user_path / (name + ".conf"));
     if(bfs::exists(local))
@@ -432,7 +435,7 @@ void load_configs(const std::string & desc,
 void MCGlobalController::GlobalConfiguration::load_controllers_configs()
 {
   // Load controller-specific configuration
-  load_configs("controller", enabled_controllers, bfs::path(mc_rtc::MC_CONTROLLER_INSTALL_PREFIX) / "etc",
+  load_configs("controller", enabled_controllers, controller_module_paths,
 #ifndef WIN32
                bfs::path(std::getenv("HOME")) / ".config/mc_rtc/controllers",
 #else
@@ -444,7 +447,7 @@ void MCGlobalController::GlobalConfiguration::load_controllers_configs()
 void MCGlobalController::GlobalConfiguration::load_plugin_configs()
 {
   // Load plugins configurations
-  load_configs("plugin", global_plugins, bfs::path(mc_rtc::MC_PLUGINS_INSTALL_PREFIX) / "etc",
+  load_configs("plugin", global_plugins, global_plugin_paths,
 #ifndef WIN32
                bfs::path(std::getenv("HOME")) / ".config/mc_rtc/plugins",
 #else

commit de4b0f404e9ceacc14353fa0770b65602f69b782

[bugfix] Load global yaml or json configuration files
Fix a bug introduced in acbb1fccf1978bedb5755b84552e3008e8bba991

src/mc_control/mc_global_controller_configuration.cpp
@@ -413,7 +413,7 @@ void load_configs(const std::string & desc,
     c.load(default_config);
     for(const auto & p : search_path)
     {
-      bfs::path global = bfs::path(p) / "etc" / (name + ".conf");
+      bfs::path global = conf_or_yaml(bfs::path(p) / "etc" / (name + ".conf"));
       if(bfs::exists(global))
       {
         LOG_INFO("Loading additional " << desc << " configuration: " << global)