932a67a118eb34b55c2e5a98dd891b39a8992d5b
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-02-21 11:53:04 +0800

    [utils] Change reference to pyside to pyqt5

--- utils/config_build_and_install.macos.sh
+++ utils/config_build_and_install.macos.sh
@@ -1,6 +1,6 @@
 export CASK_DEPENDENCIES="gfortran"
 export BREW_DEPENDENCIES="coreutils pkg-config gnu-sed wget python cmake doxygen libtool tinyxml2 geos boost eigen nanomsg yaml-cpp qt qwt"
-export PIP_DEPENDENCIES="Cython coverage nose numpy matplotlib"
+export PIP_DEPENDENCIES="Cython coverage nose numpy matplotlib pyqt5"
 if $WITH_ROS_SUPPORT && [ -z $ROS_DISTRO ]
 then
   echo "ROS support is disabled as ROS was not detected. If you have ROS, please source the setup script before running this script."
--- utils/mc_log_gui/mc_log_ui/ui/README.md
+++ utils/mc_log_gui/mc_log_ui/ui/README.md
@@ -4,5 +4,5 @@ ui files for mc_log_gui
 Generate python files with:
 
 ```bash
-pyside-uic -o [out] in.ui
+pyuic5 -o [out] in.ui
 ```
