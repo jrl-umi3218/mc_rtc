c0ab1bbe917ae766dd0657a782e803ff055eb172
Arnaud TANGUY <arnaud.tanguy@lirmm.fr>
2020-05-28 16:15:51 +0900

    [samples] Fix running of LIPMStabilizer sample, add test
    - Fix a mistake in the FSM configuration preventing the LIPMStabilizer
    sample from working (contacts defined as an empty object instead of an
    array)
    - Add unit test to ensure that this sample runs properly
    - Fix loading of robot aliases in unit tests

--- src/mc_control/samples/FSM/src/mc_fsm_controller.cpp
+++ src/mc_control/samples/FSM/src/mc_fsm_controller.cpp
@@ -8,17 +8,3 @@ FSMController::FSMController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rt
 : mc_control::fsm::Controller(rm, dt, config)
 {
 }
-
-bool FSMController::run()
-{
-  return mc_control::fsm::Controller::run();
-}
-
-void FSMController::reset(const mc_control::ControllerResetData & reset_data)
-{
-  if(robot().name() != "jvrc1")
-  {
-    LOG_ERROR_AND_THROW(std::runtime_error, "The FSM sample controller only supports the JVRC1 robot");
-  }
-  mc_control::fsm::Controller::reset(reset_data);
-}
--- src/mc_control/samples/LIPMStabilizer/src/lib.cpp
+++ src/mc_control/samples/LIPMStabilizer/src/lib.cpp
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2015-2020 CNRS-UM LIRMM, CNRS-AIST JRL
+ */
+
 #include "mc_lipm_stabilizer.h"
 
 CONTROLLER_CONSTRUCTOR("LIPMStabilizer", LIPMStabilizerController)
