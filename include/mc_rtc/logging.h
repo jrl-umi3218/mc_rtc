6a56c9057531a84107d8db73686bab84fe233376
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-05-29 11:39:48 +0800

    Put back LOG_ macros with a deprecation warning

--- include/mc_rtc/logging.h
+++ include/mc_rtc/logging.h
@@ -65,65 +65,91 @@ void success(Args &&... args)
 
 } // namespace mc_rtc
 
-/*
 #ifndef WIN32
 
- namespace mc_rtc
+namespace mc_rtc
 {
 
- constexpr auto OUT_NONE = "\033[00m";
- constexpr auto OUT_BLUE = "\033[01;34m";
- constexpr auto OUT_GREEN = "\033[01;32m";
- constexpr auto OUT_PURPLE = "\033[01;35m";
- constexpr auto OUT_RED = "\033[01;31m";
+constexpr auto OUT_NONE = "\033[00m";
+constexpr auto OUT_BLUE = "\033[01;34m";
+constexpr auto OUT_GREEN = "\033[01;32m";
+constexpr auto OUT_PURPLE = "\033[01;35m";
+constexpr auto OUT_RED = "\033[01;31m";
 
 } // namespace mc_rtc
 
-#  define LOG_ERROR(args) std::cerr << mc_rtc::OUT_RED << args << mc_rtc::OUT_NONE << std::endl;
-#  define LOG_WARNING(args) std::cerr << mc_rtc::OUT_PURPLE << args << mc_rtc::OUT_NONE << std::endl;
-#  define LOG_INFO(args) std::cout << mc_rtc::OUT_BLUE << args << mc_rtc::OUT_NONE << std::endl;
-#  define LOG_SUCCESS(args) std::cout << mc_rtc::OUT_GREEN << args << mc_rtc::OUT_NONE << std::endl;
+#  define LOG_ERROR(args)                                                                \
+    _Pragma("GCC warning \"This macro is deprecated, use mc_rtc::log::error instead\""); \
+    std::cerr << mc_rtc::OUT_RED << args << mc_rtc::OUT_NONE << "\n";
+#  define LOG_WARNING(args)                                                                \
+    _Pragma("GCC warning \"This macro is deprecated, use mc_rtc::log::warning instead\""); \
+    std::cerr << mc_rtc::OUT_PURPLE << args << mc_rtc::OUT_NONE << "\n";
+#  define LOG_INFO(args)                                                                \
+    _Pragma("GCC warning \"This macro is deprecated, use mc_rtc::log::info instead\""); \
+    std::cout << mc_rtc::OUT_BLUE << args << mc_rtc::OUT_NONE << "\n";
+#  define LOG_SUCCESS(args)                                                                \
+    _Pragma("GCC warning \"This macro is deprecated, use mc_rtc::log::success instead\""); \
+    std::cout << mc_rtc::OUT_GREEN << args << mc_rtc::OUT_NONE << "\n";
+
+#  define LOG_ERROR_AND_THROW(exception_type, args)                                                                  \
+    {                                                                                                                \
+      _Pragma("GCC warning \"This macro is deprecated, use mc_rtc::log::error_and_throw<exception_type> instead\""); \
+      std::stringstream strstrm;                                                                                     \
+      strstrm << args;                                                                                               \
+      LOG_ERROR(strstrm.str())                                                                                       \
+      throw exception_type(strstrm.str());                                                                           \
+    }
 
 #else
 
 #  include <windows.h>
- namespace mc_rtc
+namespace mc_rtc
 {
- static const HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
- constexpr auto OUT_NONE = 15;
- constexpr auto OUT_BLUE = 11;
- constexpr auto OUT_GREEN = 10;
- constexpr auto OUT_PURPLE = 13;
- constexpr auto OUT_RED = 12;
+static const HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+constexpr auto OUT_NONE = 15;
+constexpr auto OUT_BLUE = 11;
+constexpr auto OUT_GREEN = 10;
+constexpr auto OUT_PURPLE = 13;
+constexpr auto OUT_RED = 12;
 } // namespace mc_rtc
 
-#  define LOG_ERROR(args)                                       \
-    SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_RED); \
-    std::cerr << args << std::endl;                             \
+#  define __MC_RTC_STR2__(x) #  x
+#  define __MC_RTC_STR1__(x) __MC_RTC_STR2__(x)
+#  define __MC_RTC_PRAGMA_LOC__ __FILE__ "("__MC_RTC_STR1__(__LINE__) ") "
+
+#  define LOG_ERROR(args)                                                                                           \
+    __pragma(message(__MC_RTC_PRAGMA_LOC__ ": warning: this macro is deprecated, use mc_rtc::log::error instead")); \
+    SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_RED);                                                     \
+    std::cerr << args << std::endl;                                                                                 \
     SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_NONE);
 
-#  define LOG_WARNING(args)                                        \
-    SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_PURPLE); \
-    std::cerr << args << std::endl;                                \
+#  define LOG_WARNING(args)                                                                                           \
+    __pragma(message(__MC_RTC_PRAGMA_LOC__ ": warning: this macro is deprecated, use mc_rtc::log::warning instead")); \
+    SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_PURPLE);                                                    \
+    std::cerr << args << std::endl;                                                                                   \
     SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_NONE);
 
-#  define LOG_INFO(args)                                         \
-    SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_BLUE); \
-    std::cout << args << std::endl;                              \
+#  define LOG_INFO(args)                                                                                           \
+    __pragma(message(__MC_RTC_PRAGMA_LOC__ ": warning: this macro is deprecated, use mc_rtc::log::info instead")); \
+    SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_BLUE);                                                   \
+    std::cout << args << std::endl;                                                                                \
     SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_NONE);
 
-#  define LOG_SUCCESS(args)                                       \
-    SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_GREEN); \
-    std::cout << args << std::endl;                               \
+#  define LOG_SUCCESS(args)                                                                                           \
+    __pragma(message(__MC_RTC_PRAGMA_LOC__ ": warning: this macro is deprecated, use mc_rtc::log::success instead")); \
+    SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_GREEN);                                                     \
+    std::cout << args << std::endl;                                                                                   \
     SetConsoleTextAttribute(mc_rtc::hConsole, mc_rtc::OUT_NONE);
+
+#  define LOG_ERROR_AND_THROW(exception_type, args)                                                                  \
+    {                                                                                                                \
+      __pragma(                                                                                                      \
+          message(__MC_RTC_PRAGMA_LOC__                                                                              \
+                  ": warning: this macro is deprecated, use mc_rtc::log::error_and_throw<exception_type> instead")); \
+      std::stringstream strstrm;                                                                                     \
+      strstrm << args;                                                                                               \
+      LOG_ERROR(strstrm.str())                                                                                       \
+      throw exception_type(strstrm.str());                                                                           \
+    }
 
 #endif
-
-#define LOG_ERROR_AND_THROW(exception_type, args) \
-  {                                               \
-    std::stringstream strstrm;                    \
-    strstrm << args;                              \
-    LOG_ERROR(strstrm.str())                      \
-    throw exception_type(strstrm.str());          \
-  }
-*/
