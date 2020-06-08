547687f01972ebca53c697dc6bfd7cc06e6dd02c
Arnaud TANGUY <arn.tanguy@gmail.com>
2020-06-08 16:12:19 +0900

    [tasks][doc] Default properties for GazeTask and AddRemoveContact tasks

--- src/mc_tasks/GazeTask.cpp
+++ src/mc_tasks/GazeTask.cpp
@@ -96,9 +96,9 @@ namespace
 static auto registered = mc_tasks::MetaTaskLoader::register_load_function(
     "gaze",
     [](mc_solver::QPSolver & solver, const mc_rtc::Configuration & config) {
-      auto t =
-          std::make_shared<mc_tasks::GazeTask>(config("body"), Eigen::Vector3d{0, 0, 1}, config("X_b_gaze"),
-                                               solver.robots(), robotIndexFromConfig(config, solver.robots(), "gaze"));
+      auto t = std::make_shared<mc_tasks::GazeTask>(config("body"), Eigen::Vector3d{0, 0, 1},
+                                                    config("X_b_gaze", sva::PTransformd::Identity()), solver.robots(),
+                                                    robotIndexFromConfig(config, solver.robots(), "gaze"));
       t->load(solver, config);
       return t;
     });
