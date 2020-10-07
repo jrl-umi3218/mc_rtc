af7f54584fb15c6c42ce664167c5024053d865b4
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-10-07 17:20:37 +0800

    [doc] Update website bootstrap version
    Fix a bug with the carousel on Firefox

--- doc/setup-local-assets.sh
+++ doc/setup-local-assets.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env sh
 
+BOOTSTRAP_CSS="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+JQUERY="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+POPPER="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+BOOTSTRAP_JS="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+
 readonly doc_dir=`cd $(dirname $0); pwd`
 mkdir -p $doc_dir/assets/css
 mkdir -p $doc_dir/assets/js
 cd $doc_dir/assets/css
-wget -O bootstrap.min.css https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css
+wget -O bootstrap.min.css $BOOTSTRAP_CSS
 wget -O octicons.css https://cdnjs.cloudflare.com/ajax/libs/octicons/8.0.0/build.css
 cd $doc_dir/assets/js
-wget -O jquery.min.js    https://code.jquery.com/jquery-3.3.1.slim.min.js
-wget -O popper.min.js    https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js
-wget -O bootstrap.min.js https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js
+wget -O jquery.min.js    $JQUERY
+wget -O popper.min.js    $POPPER
+wget -O bootstrap.min.js $BOOTSTRAP_JS
 wget -O clipboard.min.js https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.4/clipboard.min.js
