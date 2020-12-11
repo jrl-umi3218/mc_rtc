dfd8563c567c3945cbdf4a2bfb22f8e1631ddbfa
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-12-11 15:22:17 +0800

    [mc_rbdyn] Preserve alias in parameters

--- include/mc_rbdyn/RobotLoader.h
+++ include/mc_rbdyn/RobotLoader.h
@@ -84,22 +84,25 @@ public:
       if(params.size() == 1)
       {
         guard.unlock();
-        return get_robot_module(params[0]);
+        rm = get_robot_module(params[0]);
       }
       else if(params.size() == 2)
       {
         guard.unlock();
-        return get_robot_module(params[0], params[1]);
+        rm = get_robot_module(params[0], params[1]);
       }
       else if(params.size() == 3)
       {
         guard.unlock();
-        return get_robot_module(params[0], params[1], params[2]);
+        rm = get_robot_module(params[0], params[1], params[2]);
       }
       else
       {
         mc_rtc::log::error_and_throw<mc_rtc::LoaderException>("Aliases can only handle 1 to 3 parameters");
       }
+      rm->_parameters.resize(1);
+      rm->_parameters[0] = name;
+      return rm;
     }
     else
     {
