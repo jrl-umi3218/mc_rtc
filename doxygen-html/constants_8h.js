14fd56ba93bd5e148e8b032d55ca302d50e3e204
Pierre Gergondet (Automated CI update) <pierre.gergondet@gmail.com>
2021-01-18 04:18:04 +0000

    Website from commit e383426418669292fcb9eea58b26de3e3a22c0fa

--- doxygen-html/constants_8h.js
+++ doxygen-html/constants_8h.js
@@ -1,8 +1,8 @@
 var constants_8h =
 [
     [ "EIGEN_PI", "constants_8h.html#aebdd4427aa2148313c07b0eedad449ad", null ],
-    [ "toDeg", "constants_8h.html#a357fd1ef503f568cf436fbda923472cf", null ],
-    [ "toRad", "constants_8h.html#a2ca6e1fbd563bfd79d6484a3295d08bd", null ],
+    [ "toDeg", "constants_8h.html#a9790fd3b6c0100cac0f0f7d86c4bec83", null ],
+    [ "toRad", "constants_8h.html#a42486cc28b8ed4721ee34ec2e1be876b", null ],
     [ "GRAVITY", "constants_8h.html#a04b344c34834432afcfcc1ac86bff22c", null ],
     [ "gravity", "constants_8h.html#acf271123cee2ac958b9ae0f9220ea422", null ],
     [ "PI", "constants_8h.html#a8cb2b2837ea283890b185cfef0631602", null ],
