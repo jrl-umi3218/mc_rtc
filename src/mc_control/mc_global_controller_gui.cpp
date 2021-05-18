a524d3b67ac872af6f5a909b45c410a191011945
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-05-18 16:02:32 +0800

    [mc_control] Handle gripper GUI in MCController

--- src/mc_control/mc_global_controller_gui.cpp
+++ src/mc_control/mc_global_controller_gui.cpp
@@ -23,64 +23,6 @@ void MCGlobalController::initGUI()
     gui->removeCategory({"Global", "Log"});
     gui->addElement({"Global", "Log"}, mc_rtc::gui::Button("Start a new log", [this]() { this->refreshLog(); }));
     gui->removeCategory({"Global", "Grippers"});
-    for(const auto & robot : controller().robots())
-    {
-      const auto & gi = robot.grippersByName();
-      for(const auto & g : gi)
-      {
-        auto g_ptr = g.second.get();
-        std::vector<std::string> category = {"Global", "Grippers", robot.name(), g.first};
-        gui->addElement(category, mc_rtc::gui::Button("Open", [g_ptr]() { g_ptr->setTargetOpening(1); }),
-                        mc_rtc::gui::Button("Close", [g_ptr]() { g_ptr->setTargetOpening(0); }),
-                        mc_rtc::gui::NumberSlider(
-                            "Opening percentage", [g_ptr]() { return g_ptr->opening(); },
-                            [g_ptr](double op) { g_ptr->setTargetOpening(op); }, 0, 1),
-                        mc_rtc::gui::NumberSlider(
-                            "Maximum velocity percentage", [g_ptr]() { return g_ptr->percentVMAX(); },
-                            [g_ptr](double op) { g_ptr->percentVMAX(op); }, 0, 1));
-
-        auto targetCat = category;
-        targetCat.push_back("Targets");
-        for(const auto & joint : g_ptr->activeJoints())
-        {
-          gui->addElement(targetCat, mc_rtc::gui::NumberSlider(
-                                         joint, [joint, g_ptr]() { return g_ptr->curOpening(joint); },
-                                         [joint, g_ptr](double targetOpening)
-                                         { g_ptr->setTargetOpening(joint, targetOpening); }, 0, 1));
-        }
-
-        category.push_back("Safety");
-        if(g_ptr->is_metric())
-        {
-          gui->addElement(
-              category,
-              mc_rtc::gui::NumberInput(
-                  "Actual command diff threshold [m]", [g_ptr]() { return g_ptr->actualCommandDiffTrigger(); },
-                  [g_ptr](double m) { g_ptr->actualCommandDiffTrigger(m); }),
-              mc_rtc::gui::NumberInput(
-                  "Over command limiter iterations", [g_ptr]() -> double { return g_ptr->overCommandLimitIterN(); },
-                  [g_ptr](double N) { g_ptr->overCommandLimitIterN(static_cast<unsigned int>(N)); }),
-              mc_rtc::gui::NumberInput(
-                  "Release offset [m]", [g_ptr]() { return g_ptr->releaseSafetyOffset(); },
-                  [g_ptr](double m) { g_ptr->releaseSafetyOffset(m); }));
-        }
-        else
-        {
-          gui->addElement(
-              category,
-              mc_rtc::gui::NumberInput(
-                  "Actual command diff threshold [deg]",
-                  [g_ptr]() { return mc_rtc::constants::toDeg(g_ptr->actualCommandDiffTrigger()); },
-                  [g_ptr](double deg) { g_ptr->actualCommandDiffTrigger(mc_rtc::constants::toRad(deg)); }),
-              mc_rtc::gui::NumberInput(
-                  "Over command limiter iterations", [g_ptr]() -> double { return g_ptr->overCommandLimitIterN(); },
-                  [g_ptr](double N) { g_ptr->overCommandLimitIterN(static_cast<unsigned int>(N)); }),
-              mc_rtc::gui::NumberInput(
-                  "Release offset [deg]", [g_ptr]() { return mc_rtc::constants::toDeg(g_ptr->releaseSafetyOffset()); },
-                  [g_ptr](double deg) { g_ptr->releaseSafetyOffset(mc_rtc::constants::toRad(deg)); }));
-        }
-      }
-    }
     gui->removeCategory({"Global", "Change controller"});
     gui->addElement({"Global", "Change controller"},
                     mc_rtc::gui::Label("Current controller", [this]() { return current_ctrl; }),
