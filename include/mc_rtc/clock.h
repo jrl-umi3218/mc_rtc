bd2e568f74a47bc272995990afb67499a436f95f
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-06-10 15:47:00 +0800

    [tests] Add real time option for the ticker

--- include/mc_rtc/clock.h
+++ include/mc_rtc/clock.h
@@ -10,6 +10,9 @@
 namespace mc_rtc
 {
 
+using duration_ms = std::chrono::duration<double, std::milli>;
+using duration_us = std::chrono::duration<double, std::micro>;
+
 /** mc_rtc::clock is a clock that is always steady and thus suitable for performance measurements */
 using clock = typename std::conditional<std::chrono::high_resolution_clock::is_steady,
                                         std::chrono::high_resolution_clock,
