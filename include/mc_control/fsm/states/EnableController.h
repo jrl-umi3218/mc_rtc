d1f5f17e25c72fbdbe0a0a70b91871438ed995e6
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-07-25 14:55:50 +0200

    [FSM] Add EnableController state

---
+++ include/mc_control/fsm/states/EnableController.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015-2022 CNRS-UM LIRMM, CNRS-AIST JRL
+ */
+
+#pragma once
+
+#include <mc_control/fsm/State.h>
+
+namespace mc_control
+{
+
+namespace fsm
+{
+
+/** Implements a state that switchs to a given controller
+ *
+ * This state outputs "OK" if the switch succeeds but the next state only
+ * starts when you switch back to the controller that initiated the switch.
+ *
+ * It outputs "Failed" if the switch fails (switching to an non-enabled
+ * controller).
+ */
+struct MC_CONTROL_FSM_STATE_DLLAPI EnableControllerState : State
+{
+  void start(Controller &) override;
+
+  bool run(Controller &) override
+  {
+    return true;
+  }
+
+  void teardown(Controller &) override {}
+};
+
+} // namespace fsm
+
+} // namespace mc_control
+
---
+++ src/mc_control/fsm/states/EnableController.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015-2022 CNRS-UM LIRMM, CNRS-AIST JRL
+ */
+
+#include <mc_control/fsm/Controller.h>
+#include <mc_control/fsm/states/EnableController.h>
+
+namespace mc_control
+{
+
+namespace fsm
+{
+
+void EnableControllerState::start(Controller & ctl)
+{
+  std::string next_controller = config_("NextController", std::string(""));
+  if(ctl.datastore().call<bool, const std::string &>("Global::EnableController", next_controller))
+  {
+    output("OK");
+  }
+  else
+  {
+    output("Failed");
+  }
+}
+
+} // namespace fsm
+
+} // namespace mc_control
+
+EXPORT_SINGLE_STATE("EnableController", mc_control::fsm::EnableControllerState)
