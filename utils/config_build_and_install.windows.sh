08232f0b1d44f559dd3a818dc11bda8163ed8607
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-09-05 22:57:02 +0900

    [bindings] Use pytest to run the tests
    nose does not run on Python 3.10

--- utils/config_build_and_install.windows.sh
+++ utils/config_build_and_install.windows.sh
@@ -1,5 +1,5 @@
 export ROS_DISTRO=
-export PIP_DEPENDENCIES="Cython coverage nose numpy matplotlib pyqt5"
+export PIP_DEPENDENCIES="Cython coverage nose pytest numpy matplotlib pyqt5"
 export WITH_ROS_SUPPORT="false"
 export INSTALL_PREFIX=/c/devel/install
 export SYSTEM_HAS_NINJA=OFF
