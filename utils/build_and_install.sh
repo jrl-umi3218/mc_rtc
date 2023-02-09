a90af82df78280bf4498d04c0e8b0ab4b7ee28c7
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-02-09 16:04:45 +0900

    [utils] Avoid unwanted apt upgrade

--- utils/build_and_install.sh
+++ utils/build_and_install.sh
@@ -643,7 +643,7 @@ then
       wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | ${REQUIRED_SUDO} tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
       echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release -sc) main" | ${REQUIRED_SUDO} tee /etc/apt/sources.list.d/kitware.list >/dev/null
       ${REQUIRED_SUDO} apt-get update
-      ${REQUIRED_SUDO} apt-get upgrade -y cmake cmake-curses-gui
+      ${REQUIRED_SUDO} apt-get install --only-upgrade cmake cmake-curses-gui
     fi
   fi
   if [ $OS = Debian ]
