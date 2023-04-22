ea612683db62cf1a4cf76fca684aa67e16f1bf53
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-04-22 23:43:40 +0200

    [utils/mc_log_ui] Avoid a crash when some limits are not set
    This can happen when non-canonical modules are loaded

--- utils/mc_log_gui/mc_log_ui/mc_log_tab.py
+++ utils/mc_log_gui/mc_log_ui/mc_log_tab.py
@@ -545,6 +545,8 @@ class MCLogTab(QtWidgets.QWidget):
     def_bound = [float('nan')]
     def setBounds(prefix):
         for i, jn in enumerate(self.rm.ref_joint_order()):
+          if len(bounds[0].get(jn, def_bound)) == 0:
+            continue
           if "{}qIn_limits_lower_{}".format(prefix, i) in self.data:
             self.data["{}qIn_limits_lower_{}".format(prefix, i)].fill(bounds[0].get(jn, def_bound)[0])
             self.data["{}qIn_limits_upper_{}".format(prefix, i)].fill(bounds[1].get(jn, def_bound)[0])
