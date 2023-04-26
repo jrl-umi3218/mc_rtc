d5911233afbc4b28522a42b983101c6e888718d0
Arnaud TANGUY <arn.tanguy@gmail.com>
2023-04-26 16:51:08 +0200

    [bugfix/BSplineTrajectoryTask] Use frame instead of surface

--- src/mc_tasks/BSplineTrajectoryTask.cpp
+++ src/mc_tasks/BSplineTrajectoryTask.cpp
@@ -90,11 +90,11 @@ static auto registered = mc_tasks::MetaTaskLoader::register_load_function(
         const auto & targetSurfaceName = c(has_targetSurface ? "surface" : "frame");
         const auto & robot = robotFromConfig(c, solver.robots(), "bspline_trajectory::targetFrame");
 
-        const sva::PTransformd & targetSurface = robot.surface(targetSurfaceName).X_0_s(robot);
+        const sva::PTransformd & targetFrame = robot.frame(targetSurfaceName).position();
         const Eigen::Vector3d trans = c("translation", Eigen::Vector3d::Zero().eval());
         const Eigen::Matrix3d rot = c("rotation", Eigen::Matrix3d::Identity().eval());
         sva::PTransformd offset(rot, trans);
-        finalTarget_ = offset * targetSurface;
+        finalTarget_ = offset * targetFrame;
 
         if(c.has("controlPoints"))
         {
@@ -105,7 +105,7 @@ static auto registered = mc_tasks::MetaTaskLoader::register_load_function(
           {
             const Eigen::Vector3d wp = controlPoints[i];
             sva::PTransformd X_offset(wp);
-            waypoints[i] = (X_offset * targetSurface).translation();
+            waypoints[i] = (X_offset * targetFrame).translation();
           }
         }
 
@@ -115,7 +115,7 @@ static auto registered = mc_tasks::MetaTaskLoader::register_load_function(
           for(const auto & wp : oriWaypoints)
           {
             const sva::PTransformd offset{wp.second};
-            const sva::PTransformd ori = offset * targetSurface;
+            const sva::PTransformd ori = offset * targetFrame;
             oriWp.push_back(std::make_pair(wp.first, ori.rotation()));
           }
         }
