424b20989930edcfa184f4539a89d1f199b27bbd
Pierre Gergondet <pierre.gergondet@gmail.com>
2024-01-31 19:23:32 +0900

    [utils/mc_convex_visualization] Allow to change the robot through a menu

--- utils/mc_convex_visualization.cpp
+++ utils/mc_convex_visualization.cpp
@@ -1,5 +1,7 @@
 #include <mc_rtc/gui.h>
 
+#include <mc_rtc/io_utils.h>
+
 #include <mc_rbdyn/RobotLoader.h>
 
 #include <mc_control/ControllerServer.h>
@@ -13,14 +15,11 @@
 
 struct ApplicationServer
 {
-  ApplicationServer(const mc_rbdyn::Robot & robot)
+  ApplicationServer(const std::vector<std::string> & params)
   {
     server_config.print_serving_information();
-    builder.addElement({"Robot"}, mc_rtc::gui::Robot(robot.name(), [&robot]() -> const auto & { return robot; }));
-    for(const auto & convex_it : robot.convexes())
-    {
-      mc_rbdyn::gui::addConvexToGUI(builder, {"Robot", "Collision objects"}, robot, convex_it.first);
-    }
+    available_robots = mc_rbdyn::RobotLoader::available_robots();
+    loadRobot(params);
   }
 
   virtual void publish()
@@ -42,6 +41,58 @@ struct ApplicationServer
       std::this_thread::sleep_until(now + std::chrono::milliseconds(5));
     }
   }
+
+  void loadRobot(const std::vector<std::string> & params)
+  {
+    selected_robot = -1;
+    if(!params.empty())
+    {
+      auto selected_it = std::find(available_robots.begin(), available_robots.end(), params[0]);
+      if(selected_it != available_robots.end())
+      {
+        selected_robot = static_cast<int>(std::distance(available_robots.begin(), selected_it));
+      }
+    }
+    try
+    {
+      auto rm = mc_rbdyn::RobotLoader::get_robot_module(params);
+      removeRobot();
+      robots = mc_rbdyn::Robots::make();
+      robots->load(*rm);
+      addRobot();
+    }
+    catch(...)
+    {
+      mc_rtc::log::error("Failed to load [{}]", mc_rtc::io::to_string(params));
+    }
+    setupRobotSelector();
+  }
+
+  void addRobot()
+  {
+    const auto & robot = robots->robot();
+    builder.addElement({"Robot"}, mc_rtc::gui::Robot(robot.name(), [&robot]() -> const auto & { return robot; }));
+    for(const auto & convex_it : robot.convexes())
+    {
+      mc_rbdyn::gui::addConvexToGUI(builder, {"Robot", "Collision objects"}, robot, convex_it.first);
+    }
+  }
+
+  void removeRobot() { builder.removeCategory({"Robot"}); }
+
+  void setupRobotSelector()
+  {
+    if(builder.hasElement({}, "Switch robot")) { builder.removeElement({}, "Switch robot"); }
+    builder.addElement({}, mc_rtc::gui::Form(
+                               "Switch robot",
+                               [this](const mc_rtc::Configuration & data)
+                               { this->loadRobot({data("Robot").operator std::string()}); },
+                               mc_rtc::gui::FormComboInput("Robot", true, available_robots, false, selected_robot)));
+  }
+
+  std::vector<std::string> available_robots;
+  int selected_robot = -1;
+  std::shared_ptr<mc_rbdyn::Robots> robots;
 };
 
 int main(int argc, char * argv[])
@@ -49,10 +100,7 @@ int main(int argc, char * argv[])
   std::vector<std::string> params;
   for(int i = 1; i < argc; ++i) { params.push_back(argv[i]); }
   if(params.empty()) { params.push_back("JVRC1"); }
-  auto rm = mc_rbdyn::RobotLoader::get_robot_module(params);
-  auto robots = mc_rbdyn::Robots::make();
-  robots->load(*rm);
-  ApplicationServer server(robots->robot());
+  ApplicationServer server(params);
   server.loop();
   return 0;
 }
