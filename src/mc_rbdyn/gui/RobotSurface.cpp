8a0feb92312ba4948db32ecc344bae29139a5952
Pierre Gergondet <pierre.gergondet@gmail.com>
2024-02-02 16:18:28 +0900

    [utils/mc_convex_visualization] Make it look closer to the ROS version

--- src/mc_rbdyn/gui/RobotSurface.cpp
+++ src/mc_rbdyn/gui/RobotSurface.cpp
@@ -38,7 +38,7 @@ std::vector<std::string> addSurfaceToGUI(mc_rtc::gui::StateBuilder & gui,
     std::vector<Eigen::Vector3d> points;
     points.resize(plan.points().size());
     publish_surface(mc_rtc::gui::Polygon(
-        publishName.value_or(name), mc_rtc::gui::LineConfig{mc_rtc::gui::Color::Green, 0.05},
+        publishName.value_or(name), mc_rtc::gui::LineConfig{mc_rtc::gui::Color::Green, 0.01},
         [&plan, points, get_pose]() mutable -> const std::vector<Eigen::Vector3d> &
         {
           auto pose = get_pose();
@@ -51,22 +51,28 @@ std::vector<std::string> addSurfaceToGUI(mc_rtc::gui::StateBuilder & gui,
           return points;
         }));
     // Normal publication
+    mc_rtc::gui::ArrowConfig arrow_cfg(mc_rtc::gui::Color::Blue);
+    arrow_cfg.shaft_diam = 0.01;
+    arrow_cfg.head_diam = 0.02;
+    arrow_cfg.head_len = 0.1;
     publish_surface(mc_rtc::gui::Arrow(
-        publishName.value_or(name) + "_normal", mc_rtc::gui::ArrowConfig(mc_rtc::gui::Color::Blue),
-        [get_pose]() { return get_pose().translation(); },
+        publishName.value_or(name) + "_normal", arrow_cfg, [get_pose]() { return get_pose().translation(); },
         [get_pose]() { return (sva::PTransformd(Eigen::Vector3d(0, 0, 0.2)) * get_pose()).translation(); }));
   }
   else if(surface.type() == "gripper")
   {
     const auto & gripper = dynamic_cast<const mc_rbdyn::GripperSurface &>(surface);
     size_t pid = 0;
-    mc_rtc::gui::ArrowConfig cfg{mc_rtc::gui::Color::Blue};
+    mc_rtc::gui::ArrowConfig arrow_cfg(mc_rtc::gui::Color::Blue);
+    arrow_cfg.shaft_diam = 0.005;
+    arrow_cfg.head_diam = 0.01;
+    arrow_cfg.head_len = 0.025;
     for(const auto & p : gripper.pointsFromOrigin())
     {
       auto get_start = [get_pose, &p]() { return (p * get_pose()).translation(); };
       auto get_end = [get_start, &p]() -> Eigen::Vector3d { return get_start() + p.rotation().col(2) * 0.05; };
       publish_surface(
-          mc_rtc::gui::Arrow(publishName.value_or(name) + "_" + std::to_string(pid++), cfg, get_start, get_end));
+          mc_rtc::gui::Arrow(publishName.value_or(name) + "_" + std::to_string(pid++), arrow_cfg, get_start, get_end));
     }
   }
   else { mc_rtc::log::error("[addSurfaceToGUI] Cannot handle surface type {}", surface.type()); }
