20edad5c6dee118b9ae5095949458c196c5cdfa5
Hugo L <lefevrehugo@ymail.com>
2025-03-29 22:58:00 +0100

    Add 3D and 6D options, handle default cases
    - The feasible polytope always builds a 6D polytope, either the force from the optional contact polytope and the moments limit from a basic CoP constraint, or a default friction + CoP
    - The function builds its jacobian only as force limits or with the whole 6D matrix depending on the variable dimension

--- src/mc_tvm/FeasiblePolytope.cpp
+++ src/mc_tvm/FeasiblePolytope.cpp
@@ -21,11 +21,43 @@ FeasiblePolytope::FeasiblePolytope(NewPolytopeToken, const mc_rbdyn::Contact & c
 
 void FeasiblePolytope::updatePolytope()
 {
-  const auto & feasiblePolytope = contact_.feasiblePolytope();
+  const auto feasiblePolytope = contact_.feasiblePolytope();
   if(feasiblePolytope)
   {
-    normals_ = feasiblePolytope->planeNormals;
-    offsets_ = feasiblePolytope->planeConstants;
+    // If there is a feasible polytope, build the contact wrench polytope from the wrench face matrix (moments part) and
+    // the feasible polytope (forces part)
+    // The moments are the 3 first columns, the forces the 3 next
+    // Nb of lines: force poly constraints + 4 CoP constraints
+    int nbForceConstraints = feasiblePolytope->planeConstants.size();
+    int nbMomentConstraints = 4; // XXX 4 CoP constraints, see if need to include rest of wrench face mat
+    normals_ = Eigen::MatrixXd::Zero(nbForceConstraints + nbMomentConstraints, 6);
+    offsets_ = Eigen::VectorXd::Zero(nbForceConstraints + nbMomentConstraints);
+
+    normals_.block(0, 3, nbForceConstraints, 3) = feasiblePolytope->planeNormals;
+    // XXX see how to handle the contact not being from the first surface
+    normals_.block(nbForceConstraints, 0, nbMomentConstraints, 6) = computeCoPMomentsConstraint(*contact_.r1Surface());
+
+    offsets_.segment(0, nbForceConstraints) = feasiblePolytope->planeConstants;
+    // Offsets for wrench face matrix are just zero, nothing else to do
+    // mc_rtc::log::warning("Feasible Poly: feasible detected, built as:\n{}\n and {}", normals_, offsets_.transpose());
+  }
+  else
+  {
+    // There is no feasible polytope, build a default one with just friction cone and CoP matrix
+    int nbFrictionSides = 5;
+    int nbMomentConstraints = 4;
+
+    normals_ = Eigen::MatrixXd::Zero(nbFrictionSides + nbMomentConstraints, 6);
+    offsets_ = Eigen::VectorXd::Zero(nbFrictionSides + nbMomentConstraints);
+    // Forces
+    normals_.block(0, 3, 6, 3) =
+        generatePolyhedralConeHRep(nbFrictionSides, Eigen::Matrix3d::Identity(), contact_.friction());
+    // CoP moments
+    // XXX see how to handle the contact not being from the first surface
+    normals_.block(5, 0, 4, 6) = computeCoPMomentsConstraint(*contact_.r1Surface());
+
+    // Offsets are all zero in this default case (no second member if unbound polyhedral cone)
+    // mc_rtc::log::warning("Feasible Poly: no feasible detected, built default as:\n{}", normals_);
   }
 }
 
