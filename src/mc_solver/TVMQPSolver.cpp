969f0c927210173d4c332bb0281f20fd13b8efcc
Hugo L <lefevrehugo@ymail.com>
2025-03-31 19:02:05 +0200

    Add contact wrench minimization and handle constraint size change
    - Added target tasks on force variables in ContactData (for now minimization)
    - Flag for constraint size change to remove and re add task to solver (no event in tvm to handle this for now)
    - Handle 3d or 6d correctly for polytope function second member
    - Polytope index bugfixes

--- src/mc_solver/TVMQPSolver.cpp
+++ src/mc_solver/TVMQPSolver.cpp
@@ -123,6 +123,40 @@ bool TVMQPSolver::runCommon()
     t->incrementIterInSolver();
   }
   auto start_t = mc_rtc::clock::now();
+
+  // Update force/wrench constraints if the size of the constraint changed
+  for(const auto & contactTVMQP : contactsData_)
+  {
+    int i = 0;
+    for(const auto & f1contraint : contactTVMQP.f1Constraints_)
+    {
+      mc_rtc::log::info("Force {} has {} planes constraints", i, f1contraint->task.function()->tSize());
+      auto poly_fn = std::static_pointer_cast<mc_tvm::ForceInPolytopeFunction>(f1contraint->task.function());
+      if(poly_fn->constraintSizeChanged())
+      {
+        // FIXME find a more automated way with a tvm event to avoid removing and re adding
+        mc_rtc::log::info("Updating the solver for this task");
+        problem_.remove(*f1contraint);
+        problem_.add(f1contraint);
+        poly_fn->constraintSizeChanged(false);
+      }
+      i++;
+    }
+    // Now also run on other side of the constraint
+    for(const auto & f2contraint : contactTVMQP.f2Constraints_)
+    {
+      mc_rtc::log::info("Force {} has {} planes constraints", i, f2contraint->task.function()->tSize());
+      auto poly_fn = std::static_pointer_cast<mc_tvm::ForceInPolytopeFunction>(f2contraint->task.function());
+      if(poly_fn->constraintSizeChanged())
+      {
+        mc_rtc::log::info("Updating the solver for this task");
+        problem_.remove(*f2contraint);
+        problem_.add(f2contraint);
+        poly_fn->constraintSizeChanged(false);
+      }
+      i++;
+    }
+  }
   auto r = solver_.solve(problem_);
   solve_dt_ = mc_rtc::clock::now() - start_t;
   return r;
@@ -285,7 +319,10 @@ void TVMQPSolver::addDynamicsConstraint(mc_solver::DynamicsConstraint * dyn)
       // FIXME Debug mc_rbdyn::intersection
       // auto s1Points = mc_rbdyn::intersection(s1, s2);
       const auto & s1Points = s1.points();
-      if(isR1) { addContactToDynamics(r1.name(), f1, s1Points, data.f1_, data.f1Constraints_, *contact, 1.0); }
+      if(isR1)
+      {
+        addContactToDynamics(r1.name(), f1, s1Points, data.f1_, data.f1Constraints_, data.f1Targets_, *contact, 1.0);
+      }
       if(isR2)
       {
         std::vector<sva::PTransformd> s2Points;
@@ -293,7 +330,7 @@ void TVMQPSolver::addDynamicsConstraint(mc_solver::DynamicsConstraint * dyn)
         auto X_b2_b1 =
             r1.mbc().bodyPosW[r1.bodyIndexByName(f1.body())] * r2.mbc().bodyPosW[r2.bodyIndexByName(f2.body())].inv();
         for(const auto & X_b1_p : s1Points) { s2Points.push_back(X_b1_p * X_b2_b1); }
-        addContactToDynamics(r2.name(), f2, s2Points, data.f2_, data.f2Constraints_, *contact, -1.0);
+        addContactToDynamics(r2.name(), f2, s2Points, data.f2_, data.f2Constraints_, data.f2Targets_, *contact, -1.0);
       }
     }
   }
@@ -319,17 +356,20 @@ void TVMQPSolver::removeDynamicsConstraint(mc_solver::DynamicsConstraint * dyn)
     const auto & contact = contacts_[i];
     auto & data = contactsData_[i];
     auto clearContacts = [&](const std::string & robot, tvm::VariableVector & forces,
-                             std::vector<tvm::TaskWithRequirementsPtr> & constraints)
+                             std::vector<tvm::TaskWithRequirementsPtr> & constraints,
+                             std::vector<tvm::TaskWithRequirementsPtr> & targets)
     {
       if(robot != r.name()) { return; }
       for(auto & c : constraints) { problem_.remove(*c); }
       constraints.clear();
+      for(auto & t : targets) { problem_.remove(*t); }
+      targets.clear();
       forces = tvm::VariableVector();
     };
     const auto & r1 = robot(contact->r1Index());
-    clearContacts(r1.name(), data.f1_, data.f1Constraints_);
+    clearContacts(r1.name(), data.f1_, data.f1Constraints_, data.f1Targets_);
     const auto & r2 = robot(contact->r2Index());
-    clearContacts(r2.name(), data.f2_, data.f2Constraints_);
+    clearContacts(r2.name(), data.f2_, data.f2Constraints_, data.f2Targets_);
   }
 }
 
@@ -338,6 +378,7 @@ void TVMQPSolver::addContactToDynamics(const std::string & robot,
                                        const std::vector<sva::PTransformd> & points,
                                        tvm::VariableVector & forces,
                                        std::vector<tvm::TaskWithRequirementsPtr> & constraints,
+                                       std::vector<tvm::TaskWithRequirementsPtr> & targets,
                                        const mc_rbdyn::Contact & contact,
                                        double dir)
 {
@@ -363,20 +404,19 @@ void TVMQPSolver::addContactToDynamics(const std::string & robot,
 
   // if(contact.feasiblePolytope())
   // {
-  mc_rtc::log::info("Adding polytope constraints");
-  // The "constraints" are only acting on the variables ! so 1 force var = 1 constraint, not planes size
+  // mc_rtc::log::info("Adding polytope constraints");
 
   for(int i = 0; i < forces.numberOfVariables(); ++i)
   {
-    // it->second->removeFromSolverImpl(*this);
     auto & f = forces[i];
     auto polyFunction = std::make_shared<mc_tvm::ForceInPolytopeFunction>(contact, f, dir);
     // We want the force to stay inside of the polytope so the distance value should stay negative
     constraints.push_back(
         problem_.add(polyFunction <= 0., tvm::task_dynamics::None(), {tvm::requirements::PriorityLevel(0)}));
-    auto & dyn = it->second->dynamicFunction();
-    // dyn.addForceConstraintDep(polyFunction);
-    // it->second->addToSolverImpl(*this);
+    // Add a minimization on the force variable with a low weight
+    // TODO maybe find a way to parametrize a desired anisotropic weight from the mc_rbdyn::Contact ?
+    // TODO write a linear function to target zero (minimize) or a specific target
+    targets.push_back(problem_.add(f == 0.0, {tvm::requirements::PriorityLevel(1), tvm::requirements::Weight(0.0001)}));
   }
 
   // }
@@ -384,7 +424,6 @@ void TVMQPSolver::addContactToDynamics(const std::string & robot,
   // {
   //   const auto frictionCone = discretizedFrictionCone(contact.friction());
   //   mc_rtc::log::info("Adding simple friction cone constraints: {} constraints", frictionCone.rows());
-  //   mc_rtc::log::info("Before this cone problem had {} constraints", problem_.constraints().size());
   //   for(int i = 0; i < forces.numberOfVariables(); ++i)
   //   {
   //     auto & f = forces[i];
@@ -394,8 +433,6 @@ void TVMQPSolver::addContactToDynamics(const std::string & robot,
   //     mc_rtc::log::info("The corresponding jacobian for this task is\n{}",
   //                       problem_.constraint(*constraints.back().get())->jacobian(*f.get()));
   //   }
-  //   mc_rtc::log::info("Problem has now {} constraints over {} variables", problem_.constraints().size(),
-  //                     problem_.variables().numberOfVariables());
   // }
 }
 
@@ -474,21 +511,24 @@ void TVMQPSolver::addContact(const mc_rbdyn::Contact & contactTmp)
   auto addContactForce = [&addedContact, this](const std::string & robot, const mc_rbdyn::RobotFrame & frame,
                                                const std::vector<sva::PTransformd> & points,
                                                tvm::VariableVector & forces,
-                                               std::vector<tvm::TaskWithRequirementsPtr> & constraints, double dir)
-  { addContactToDynamics(robot, frame, points, forces, constraints, addedContact, dir); };
+                                               std::vector<tvm::TaskWithRequirementsPtr> & constraints,
+                                               std::vector<tvm::TaskWithRequirementsPtr> & targets, double dir)
+  { addContactToDynamics(robot, frame, points, forces, constraints, targets, addedContact, dir); };
 
   // FIXME These points computation are a waste of time if they are not needed
   // FIXME Debug mc_rbdyn::intersection
   // auto s1Points = mc_rbdyn::intersection(s1, s2);
   auto s1Points = s1.points();
-  addContactForce(r1.name(), f1, s1Points, data.f1_, data.f1Constraints_, 1.0);
+  mc_rtc::log::info("Dynamics change: adding contact force direction 1");
+  addContactForce(r1.name(), f1, s1Points, data.f1_, data.f1Constraints_, data.f1Targets_, 1.0);
   std::vector<sva::PTransformd> s2Points;
   s2Points.reserve(s1Points.size());
   auto X_b2_b1 =
       r1.mbc().bodyPosW[r1.bodyIndexByName(f1.body())] * r2.mbc().bodyPosW[r2.bodyIndexByName(f2.body())].inv();
   std::transform(s1Points.begin(), s1Points.end(), std::back_inserter(s2Points),
                  [&](const auto & X_b1_p) { return X_b1_p * X_b2_b1; });
-  addContactForce(r2.name(), f2, s2Points, data.f2_, data.f2Constraints_, -1.0);
+  mc_rtc::log::info("Dynamics change: adding contact force direction -1");
+  addContactForce(r2.name(), f2, s2Points, data.f2_, data.f2Constraints_, data.f2Targets_, -1.0);
 }
 
 auto TVMQPSolver::removeContact(size_t idx) -> ContactIterator
@@ -513,6 +553,8 @@ auto TVMQPSolver::removeContact(size_t idx) -> ContactIterator
   }
   for(const auto & c : data.f1Constraints_) { problem_.remove(*c); }
   for(const auto & c : data.f2Constraints_) { problem_.remove(*c); }
+  for(const auto & t : data.f1Targets_) { problem_.remove(*t); }
+  for(const auto & t : data.f2Targets_) { problem_.remove(*t); }
   if(data.contactConstraint_)
   {
     problem_.remove(*data.contactConstraint_);
