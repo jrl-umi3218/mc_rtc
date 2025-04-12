58212fd703260cd9879c11b68fea0647eac86116
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-12 09:31:23 +0200

    bindings: Generate stubs file for intellisense

--- binding/nanobind/tests/mc_rtc/test_configuration.py
+++ binding/nanobind/tests/mc_rtc/test_configuration.py
@@ -1,4 +1,5 @@
 import mc_rtc
+import numpy as np
 
 c = mc_rtc.Configuration()
 c.add_null("null_key")
@@ -6,3 +7,6 @@ c.array("array_reserve", 20)
 arr = c.array("array")
 arr.push(42)
 arr.push(42.)
+
+vec2 = np.array([1, 2])
+arr.push(vec2)
