9c53762e8a30d8fe7896556fe3e60ef7cda8c14f
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-15 18:16:38 +0200

    Working lsp completion and clang format

--- binding/nanobind/include/mc_rtc_python/mc_rtc_module.h
+++ binding/nanobind/include/mc_rtc_python/mc_rtc_module.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <nanobind/nanobind.h>
 
-namespace mc_rtc_python {
+namespace mc_rtc_python
+{
 
-  void bind_configuration(nanobind::module_ & m);
+void bind_configuration(nanobind::module_ & m);
 
 }
