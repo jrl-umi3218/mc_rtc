e920f91cb7f3309a5269b850caf3e488b85203a7
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-15 20:18:46 +0200

    bindings: use typelist to bind Configuration::add for most types

--- binding/nanobind/include/typelist_visitor.h
+++ binding/nanobind/include/typelist_visitor.h
@@ -1,13 +1,5 @@
 #pragma once
 
-// XXX: Make it independent from nanobind and move to mc_rtc
-
-#include <mc_rtc/Configuration.h>
-#include <mc_rtc/type_name.h>
-#include <nanobind/nanobind.h>
-
-namespace nb = nanobind;
-
 namespace mc_rtc
 {
 namespace internal
@@ -15,7 +7,7 @@ namespace internal
 // Compile-time iteration over a typelist
 // Applies a Visitor for each type T
 
-template<typename... Args>
+template<typename... params>
 struct TypeList
 {
 };
@@ -27,46 +19,46 @@ namespace Detail
 template<typename...>
 struct TypeVisitor;
 
-// Pattern match to extract the TypeLists types into Args
-template<template<typename...> class Sequence, typename... Args>
-struct TypeVisitor<Sequence<Args...>>
+// Pattern match to extract the TypeLists types into params
+template<template<typename...> class Sequence, typename... params>
+struct TypeVisitor<Sequence<params...>>
 {
 
-  template<typename F>
-  static constexpr void Visit(nb::module_ & m)
+  template<typename F, typename Param>
+  static constexpr void Visit(Param & param)
   {
     // Start recursive visitation for each type
-    DoVisit<F, Args...>(m);
+    DoVisit<F, Param, params...>(param);
   }
 
   // Allow empty sequence
-  template<typename F>
-  static constexpr void DoVisit(nb::module_ & m)
+  template<typename F, typename Param>
+  static constexpr void DoVisit(Param & param)
   {
   }
 
   // Base case: one type, invoke functor
-  template<typename F, typename Head>
-  static constexpr void DoVisit(nb::module_ & m)
+  template<typename F, typename Param, typename Head>
+  static constexpr void DoVisit(Param & param)
   {
-    F::template Visit<Head>(m);
+    F::template Visit<Head, Param>(param);
   }
 
   // At least [Head, Next], Tail... can be empty
-  template<typename F, typename Head, typename Next, typename... Tail>
-  static constexpr void DoVisit(nb::module_ & m)
+  template<typename F, typename Param, typename Head, typename Next, typename... Tail>
+  static constexpr void DoVisit(Param & param)
   {
     // Visit head and recurse visitation on rest
-    DoVisit<F, Head>(m), DoVisit<F, Next, Tail...>(m);
+    DoVisit<F, Param, Head>(param), DoVisit<F, Param, Next, Tail...>(param);
   }
 };
 } // namespace Detail
 
 // Invokes the functor with every type, this code generation is done at compile time
-template<typename Sequence, typename F>
-constexpr void ForEach(nb::module_ & m)
+template<typename Sequence, typename F, typename Param>
+constexpr void ForEach(Param & param)
 {
-  Detail::TypeVisitor<Sequence>::template Visit<F>(m);
+  Detail::TypeVisitor<Sequence>::template Visit<F, Param>(param);
 }
 
 // User defined visitor that checks type sizes with specializations for int and double
