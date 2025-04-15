dd9b8db8149a94518ed97f09cc2b0f917dc14093
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-15 18:16:38 +0200

    Working lsp completion and clang format

--- binding/nanobind/src/mc_rtc/mc_rtc_module.cpp
+++ binding/nanobind/src/mc_rtc/mc_rtc_module.cpp
@@ -1,12 +1,13 @@
-#include <nanobind/nanobind.h>
 #include <mc_rtc_python/mc_rtc_module.h>
+#include <nanobind/nanobind.h>
 
 namespace nb = nanobind;
 using namespace nb::literals;
 
-NB_MODULE(mc_rtc_python, m) {
-    m.doc() = "This is a \"hello world\" example with nanobind";
-    m.def("add", [](int a, int b, int c) { return a + b; }, "a"_a, "b"_a, "c"_a);
+NB_MODULE(_mc_rtc, m)
+{
+  m.doc() = "This is a \"hello world\" example with nanobind";
+  m.def("add", [](int a, int b, int c) { return a + b; }, "a"_a, "b"_a, "c"_a);
 
-    mc_rtc_python::bind_configuration(m);
+  mc_rtc_python::bind_configuration(m);
 }
--- binding/nanobind/tests/test_basic.py
+++ binding/nanobind/tests/test_basic.py
@@ -1,4 +1,5 @@
 import mc_rtc as m
 
+
 def test_add():
     assert m.add(1, 2) == 3
