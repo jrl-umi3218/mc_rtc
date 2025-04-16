2deed93be25b78e49a5c5124f2154b47b5438606
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-16 18:24:33 +0200

    binding: Fix stub generation

--- binding/nanobind/src/mc_rtc/__init__.py
+++ binding/nanobind/src/mc_rtc/__init__.py
@@ -1,1 +1,7 @@
-from ._mc_rtc import add, Configuration, __doc__
+#
+# In order for stubs to work correctly, we need to import each symbol as
+# from .mc_rtc_python import Symbol as Symbol
+#
+# The "as Symbol" is used to bring the otherwise private symols into the public namespace
+
+from .mc_rtc_python import Configuration as Configuration
