892826724438d2c7fb35bb1cc0092a412ae5643e
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-16 18:25:09 +0200

    binding(mc_rtc): Bind add and get with typelist_visitor

--- binding/nanobind/src/mc_rtc/mc_rtc_module.cpp
+++ binding/nanobind/src/mc_rtc/mc_rtc_module.cpp
@@ -4,10 +4,9 @@
 namespace nb = nanobind;
 using namespace nb::literals;
 
-NB_MODULE(_mc_rtc, m)
+NB_MODULE(mc_rtc_python, m)
 {
   m.doc() = "This is a \"hello world\" example with nanobind";
-  m.def("add", [](int a, int b, int c) { return a + b; }, "a"_a, "b"_a, "c"_a);
 
   mc_rtc_python::bind_configuration(m);
 }
