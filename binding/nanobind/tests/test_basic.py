2ddbe4569cb677208929312338dc6a0336c0a155
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-16 20:18:09 +0200

    bindings(wip): bind most elements

--- binding/nanobind/tests/test_basic.py
+++
@@ -1,5 +0,0 @@
-import mc_rtc as m
-
-
-def test_add():
-    assert m.add(1, 2) == 3
--- binding/nanobind/tests/test_configuration.py
+++ binding/nanobind/tests/test_configuration.py
@@ -3,7 +3,6 @@ import mc_rtc
 
 # import pytest
 
-
 def test_configuration_manual():
     c = mc_rtc.Configuration()
     c.add_null("null_key")
@@ -12,6 +11,7 @@ def test_configuration_manual():
     arr = c.array("empty_array")
     assert c.has("empty_array")
     arr.push(3.5)
+    assert arr.isArray()
 
     np_vec2 = np.array([1.0, 2.0])
     c.add("vec2", np_vec2)
@@ -20,6 +20,7 @@ def test_configuration_manual():
     float_ref = 3.0
     c.add("float", float_ref)
     assert c.has("float")
+    assert c["float"].isNumeric()
     assert c.get_double("float") == float_ref
 
     # get non-existing element with default_value
@@ -27,7 +28,7 @@ def test_configuration_manual():
     assert c.get_double("non_existing", 42.0) == 42.0
     c2_ref = mc_rtc.Configuration()
     c.add("config", c2_ref)
-    c("config").add("float", 42.0)
+    c["config"].add("float", 42.0)
     assert c.has("config")
     c2 = c.get_Configuration("config")
     assert c2.get_double("float") == 42.0
