6607e4f5f481c34e0c231889938f465e89cdc30b
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-22 18:00:53 +0200

    bindings(mc_rbdyn): Setup mc_rbdyn submodule

--- binding/nanobind/src/mc_rtc/__init__.py
+++ binding/nanobind/src/mc_rtc/__init__.py
@@ -4,6 +4,10 @@
 #
 # The "as Symbol" is used to bring the otherwise private symols into the public namespace
 
-from .mc_rtc_python import Configuration as Configuration
+# from .mc_rtc_python import Configuration as Configuration
+# from .mc_rtc_python import mc_rbdyn as mc_rbdyn
+from .mc_rtc_python import *
 
-__all__ = ["Configuration"]
+# FIXME: how to import this for stubs
+# from .mc_rtc_python.mc_rbdyn import RobotModule as RobotModule
+__all__ = ["Configuration", "mc_rbdyn"]
