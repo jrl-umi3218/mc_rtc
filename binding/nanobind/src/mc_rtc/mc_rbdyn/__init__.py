e57e99f7e4e408662831ff381a05d02e0ae59aa1
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-25 17:34:13 +0200

    bindings(packaging): Import symbols into mc_rtc parent module
    This avoids the use of mc_rtc._mc_rtc

--- binding/nanobind/src/mc_rtc/mc_rbdyn/__init__.py
+++ binding/nanobind/src/mc_rtc/mc_rbdyn/__init__.py
@@ -1,9 +1,3 @@
-from ..mc_rtc_python.mc_rbdyn import RobotModule as RobotModule
-from ..mc_rtc_python.mc_rbdyn import RobotLoader as RobotLoader
-
-# from ..mc_rtc_python.mc_rbdyn import FrameDescription as FrameDescription
-from ..mc_rtc_python.mc_rbdyn import Collision as Collision
-from ..mc_rtc_python.mc_rbdyn import Flexibility as Flexibility
-from ..mc_rtc_python.mc_rbdyn import Mimic as Mimic
+from .._mc_rtc import mc_rbdyn
 
 __all__ = ["RobotModule", "RobotLoader", "Collision", "Flexibility", "Mimic"]
