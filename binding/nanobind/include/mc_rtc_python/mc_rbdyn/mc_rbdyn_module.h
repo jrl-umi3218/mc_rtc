eb9b02262ac1776b94f3a10c38786695bb78f426
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-26 19:44:12 +0200

    wip(bindings): Start binding Robots/Robot

--- binding/nanobind/include/mc_rtc_python/mc_rbdyn/mc_rbdyn_module.h
+++ binding/nanobind/include/mc_rtc_python/mc_rbdyn/mc_rbdyn_module.h
@@ -8,5 +8,7 @@ void bind_mc_rbdyn_module(nanobind::module_ & m);
 void bind_ForceSensorCalibData(nanobind::module_ & m);
 void bind_ForceSensor(nanobind::module_ & m);
 void bind_RobotModule(nanobind::module_ & m);
+void bind_LoadRobotParameters(nanobind::module_ & m);
+void bind_Robots(nanobind::module_ & m);
 void bind_Robot(nanobind::module_ & m);
 } // namespace mc_rtc_python
