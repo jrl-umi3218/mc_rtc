c31b600b419baaa5f0ca8793dfe556eb8adaf865
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-07-24 14:34:13 +0200

    bindings(mc_rbdyn): bind RobotFrame

--- binding/nanobind/src/mc_rtc_module/mc_rbdyn/bind_Robot.cpp
+++ binding/nanobind/src/mc_rtc_module/mc_rbdyn/bind_Robot.cpp
@@ -20,6 +20,7 @@ namespace nb = nanobind;
 using namespace nb::literals;
 using Robot = mc_rbdyn::Robot;
 using RobotModule = mc_rbdyn::RobotModule;
+using RobotFrame = mc_rbdyn::RobotFrame;
 using BodySensor = mc_rbdyn::BodySensor;
 using ForceSensor = mc_rbdyn::ForceSensor;
 using JointSensor = mc_rbdyn::JointSensor;
@@ -27,6 +28,18 @@ using Device = mc_rbdyn::Device;
 
 namespace mc_rtc_python
 {
+
+template<typename Class, typename Ret, typename... Args>
+void bind_ref_cref_accessor(nb::class_<Class> & c,
+                            const std::string & name,
+                            Ret & (Class::*fun)(),
+                            const Ret & (Class::*cfun)() const,
+                            Args &&... args)
+{
+  c.def((name + "_const").c_str(), [&](Class & self) -> const Ret & { return std::invoke(cfun, self); },
+        std::forward<Args>(args)...);
+  c.def(name.c_str(), [&](Class & self) -> Ret & { return std::invoke(fun, self); }, std::forward<Args>(args)...);
+}
 
 void bind_Robot(nb::module_ & m)
 {
@@ -102,6 +115,87 @@ void bind_Robot(nb::module_ & m)
       .def("hasBody", &Robot::hasBody, "name"_a, ":returns: True if the robot has a body named **name**")
       .def("hasFrame", &Robot::hasFrame, "name"_a, ":returns: True if the robot has a frame named **name**");
 
+  // TODO: bind frames
+  robot.def(
+      "frame", [](Robot & self, const std::string & name) -> const RobotFrame & { return self.frame(name); },
+      nb::rv_policy::reference, "name"_a, R"(
+   Access the frame named **name** (const)
+
+  :throws: If the frame does not exist)");
+  robot.def(
+      "frame", [](Robot & self, const std::string & name) -> RobotFrame & { return self.frame(name); },
+      nb::rv_policy::reference, "name"_a, R"(
+   Access the frame named **name**
+
+  :throws: If the frame does not exist)");
+  robot.def("frames", &Robot::frames, "Returns the list of available frames in this robot");
+
+  robot
+      .def("jointIndexByName", &Robot::jointIndexByName, "name"_a,
+           R"(
+  :returns: the joint index of joint named \name
+
+  :throws: If the joint does not exist within the robot.
+          )")
+      .def("jointIndexInMBC", &Robot::jointIndexInMBC, "refJointOrderIndex"_a,
+           R"(
+  :returns: the joint index in the mbc of the joint with index jointIndex in
+  refJointOrder
+  :note: Joint indices can be -1 for joints present in refJointOrder but not
+  in the robot's mbc (such as filtered joints in some robot modules)
+  :param refJointOrderIndex: Joint index in refJointOrder
+  :throws: If jointIndex >= refJointOrder.size()
+              )")
+      .def("bodyIndexByName", &Robot::bodyIndexByName, "name"_a,
+           R"(
+  :returns: the body index of joint named **name**
+
+  :throws: If the body does not exist within the robot.
+              )");
+
+  // TODO: bind mb/mbc/mbg
+  robot.def("mbc", nb::overload_cast<>(&Robot::mbc), nb::rv_policy::reference,
+            "Access the MultiBodyConfig of the robot's mb");
+
+  robot
+      .def("q_const", nb::overload_cast<>(&Robot::q, nb::const_),
+           R"(Equivalent to :py:attr:`rbdyn.MultiBodyConfig.q` returned by :py:func:`mbc` (const))")
+      .def("q", nb::overload_cast<>(&Robot::q),
+           R"(Equivalent to :py:attr:`rbdyn.MultiBodyConfig.q` returned by :py:func:`mbc`)")
+      .def("alpha_const", nb::overload_cast<>(&Robot::alpha, nb::const_),
+           R"(Equivalent to :py:attr:`mbc().alpha` (const))")
+      .def("alpha", nb::overload_cast<>(&Robot::alpha), R"(Equivalent to :py:attr:`mbc().alpha`)")
+      .def("alphaD_const", nb::overload_cast<>(&Robot::alphaD, nb::const_),
+           R"(Equivalent to :py:attr:`mbc().alphaD` (const))")
+      .def("alphaD", nb::overload_cast<>(&Robot::alphaD), R"(Equivalent to :py:attr:`mbc().alphaD`)")
+      .def("jointTorque_const", nb::overload_cast<>(&Robot::jointTorque, nb::const_),
+           R"(Equivalent to :py:attr:`mbc().jointTorque` (const))")
+      .def("jointTorque", nb::overload_cast<>(&Robot::jointTorque), R"(Equivalent to :py:attr:`mbc().jointTorque`)")
+      .def("controlTorque_const", nb::overload_cast<>(&Robot::controlTorque, nb::const_),
+           R"(Access the desired control torque (const))")
+      .def("controlTorque", nb::overload_cast<>(&Robot::controlTorque), R"(Access the desired control torque)");
+
+  robot
+      .def("bodyPosW_const", nb::overload_cast<>(&Robot::bodyPosW, nb::const_),
+           R"(Equivalent to :py:attr:`mbc().bodyPosW` (const))")
+      .def("bodyPosW", nb::overload_cast<>(&Robot::bodyPosW), R"(Equivalent to :py:attr:`mbc().bodyPosW`)")
+      .def("bodyVelW_const", nb::overload_cast<>(&Robot::bodyPosW, nb::const_),
+           R"(Equivalent to :py:attr:`mbc().bodyVelW` (const))")
+      .def("bodyVelW", nb::overload_cast<>(&Robot::bodyPosW), R"(Equivalent to :py:attr:`mbc().bodyVelW`)")
+      .def("bodyVelB_const", nb::overload_cast<>(&Robot::bodyVelB, nb::const_),
+           R"(Equivalent to :py:attr:`mbc().bodyVelB` (const))")
+      .def("bodyVelB", nb::overload_cast<>(&Robot::bodyVelB), R"(Equivalent to :py:attr:`mbc().bodyVelB`)")
+      .def("bodyAccW_const", nb::overload_cast<>(&Robot::bodyPosW, nb::const_),
+           R"(Equivalent to :py:attr:`mbc().bodyAccW` (const))")
+      .def("bodyAccW", nb::overload_cast<>(&Robot::bodyPosW), R"(Equivalent to :py:attr:`mbc().bodyAccW`)")
+      .def("bodyAccB_const", nb::overload_cast<>(&Robot::bodyPosW, nb::const_),
+           R"(Equivalent to :py:attr:`mbc().bodyAccB` (const))")
+      .def("bodyAccB", nb::overload_cast<>(&Robot::bodyPosW), R"(Equivalent to :py:attr:`mbc().bodyAccB`)");
+  ;
+
+  bind_ref_cref_accessor<>(robot, "bodyPosW_Test", &Robot::bodyPosW, &Robot::bodyPosW,
+                           R"(Equivalent to :py:attr:`mbc().bodyPosW`)");
+
   robot.def(
       "devices",
       [](Robot & self) -> std::vector<Device *>
@@ -156,8 +250,6 @@ void bind_Robot(nb::module_ & m)
 
   :note: The grippers does not exist until :py:class:`mc_control.MCController` has been created. Use :py:attr:`RobotModule.gripper` instead.
               )");
-
-  robot.def("mbc", [](Robot & self) -> rbd::MultiBodyConfig & { return self.mbc(); });
 }
 
 } // namespace mc_rtc_python
