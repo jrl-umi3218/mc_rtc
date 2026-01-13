106f5e4fb78888ddb4926cb32672711d89dac9c6
Arnaud Tanguy (Automated CI update) <arn.tanguy@gmail.com>
2026-01-13 03:26:53 +0000

    Website from commit 22343cdb1c69215b10b759c086c86a4d63ef638e

--- doxygen-html/structfmt_1_1formatter_3_01mc__solver_1_1QPSolver_1_1Backend_01_4.js
+++ doxygen-html/structfmt_1_1formatter_3_01mc__solver_1_1QPSolver_1_1Backend_01_4.js
@@ -1,4 +1,4 @@
 var structfmt_1_1formatter_3_01mc__solver_1_1QPSolver_1_1Backend_01_4 =
 [
-    [ "format", "structfmt_1_1formatter_3_01mc__solver_1_1QPSolver_1_1Backend_01_4.html#a83d6c4d0b120a989cf5ffc10eb19810a", null ]
+    [ "format", "structfmt_1_1formatter_3_01mc__solver_1_1QPSolver_1_1Backend_01_4.html#a0ad927e0dbcaad95803bf81571483ba3", null ]
 ];
--- jp/doxygen-html/structfmt_1_1formatter_3_01mc__rtc_1_1Configuration_01_4.js
+++ jp/doxygen-html/structfmt_1_1formatter_3_01mc__rtc_1_1Configuration_01_4.js
@@ -1,4 +1,4 @@
 var structfmt_1_1formatter_3_01mc__rtc_1_1Configuration_01_4 =
 [
-    [ "format", "structfmt_1_1formatter_3_01mc__rtc_1_1Configuration_01_4.html#aa2d4dc5f13d6cea93bca974815edfe1d", null ]
+    [ "format", "structfmt_1_1formatter_3_01mc__rtc_1_1Configuration_01_4.html#aa4a2a0ac74be834b2413d61b6003cdaf", null ]
 ];
