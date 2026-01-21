64f477a9a8665615d6ae6f5d86c9762ae681fd8f
chuoru <tranvietthanh0101@gmail.com>
2026-01-21 16:23:30 +0900

    fix(pre-commit): remove mixed line ending for add_collision_constraint.cpp and other html files

--- doc/_i18n/en/tutorials/introduction/dual-arm-controller/add_collision_constraint.cpp
+++ doc/_i18n/en/tutorials/introduction/dual-arm-controller/add_collision_constraint.cpp
@@ -2,6 +2,4 @@
 double iDist = 0.1;
 double sDist = 0.05;
 double damping = 0.0;
-addCollisions(
-  "ur5e", "kinova_default", {{"*", "*", iDist, sDist, damping}}
-);
+addCollisions("ur5e", "kinova_default", {{"*", "*", iDist, sDist, damping}});
