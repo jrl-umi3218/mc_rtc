ef98eeedc126a38e8bb360d22896331389e02c13
chuoru <tranvietthanh0101@gmail.com>
2026-02-25 01:10:47 +0900

    fix(pre-commit): apply pre-commit

--- doc/_includes/tutorials/introduction/mobile-arm-controller/controller.cpp
+++ doc/_includes/tutorials/introduction/mobile-arm-controller/controller.cpp
@@ -1,18 +1,14 @@
 #include "MobileArmController.h"
 
 #include <mc_rbdyn/RobotLoader.h>
-#include <thread>
 #include <chrono>
+#include <thread>
 
-MobileArmController::MobileArmController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration &config) 
-: mc_control::MCController(
-  {
-    rm, 
-    mc_rbdyn::RobotLoader::get_robot_module("dingo"),
-    mc_rbdyn::RobotLoader::get_robot_module("env/door"),
-    mc_rbdyn::RobotLoader::get_robot_module("env/ground")
-  }, 
-  dt) 
+MobileArmController::MobileArmController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
+: mc_control::MCController({rm, mc_rbdyn::RobotLoader::get_robot_module("dingo"),
+                            mc_rbdyn::RobotLoader::get_robot_module("env/door"),
+                            mc_rbdyn::RobotLoader::get_robot_module("env/ground")},
+                           dt)
 {
   solver().addConstraintSet(contactConstraint);
   solver().addConstraintSet(selfCollisionConstraint);
@@ -35,38 +31,33 @@ MobileArmController::MobileArmController(mc_rbdyn::RobotModulePtr rm, double dt,
   mc_rtc::log::success("MobileArmController init done ");
 }
 
-bool MobileArmController::run() 
+bool MobileArmController::run()
 {
-  if (phase_ == APPROACH && count < 1000) 
+  if(phase_ == APPROACH && count < 1000) { count++; }
+  else if(phase_ == APPROACH && dingoEndEffectorTask_->eval().norm() < 1e-5
+          && dingoEndEffectorTask_->speed().norm() < 1e-5)
   {
-    count++;
-  } 
-  else if (phase_ == APPROACH && dingoEndEffectorTask_->eval().norm() < 1e-5 
-    && dingoEndEffectorTask_->speed().norm() < 1e-5) 
-  { 
     solver().removeTask(postureTask.get());
     handTask_->reset();
     solver().addTask(handTask_);
-    handTask_->target(sva::PTransformd(Eigen::Vector3d(0, 0, -0.05)) *
-                  robots().robot(2).surfacePose("Handle"));
+    handTask_->target(sva::PTransformd(Eigen::Vector3d(0, 0, -0.05)) * robots().robot(2).surfacePose("Handle"));
     phase_ = HANDLE;
-  } 
-  else if (phase_ == HANDLE && handTask_->eval().norm() < 0.1 
-  && handTask_->speed().norm() < 1e-4) 
+  }
+  else if(phase_ == HANDLE && handTask_->eval().norm() < 0.1 && handTask_->speed().norm() < 1e-4)
   {
     addContact({"ur5e", "door", "Tool", "Handle"});
     solver().removeTask(handTask_);
     postureTask->reset();
     doorPostureTask_->target({{"handle", {-1.0}}});
     phase_ = OPEN;
-  } 
-  else if (phase_ == OPEN && doorPostureTask_->eval().norm() < 0.01) 
+  }
+  else if(phase_ == OPEN && doorPostureTask_->eval().norm() < 0.01)
   {
     solver().removeTask(dingoEndEffectorTask_);
     doorPostureTask_->target({{"door", {1.57}}});
     phase_ = DONE;
-  } 
-  else if (phase_ == DONE && doorPostureTask_->eval().norm() < 0.01) 
+  }
+  else if(phase_ == DONE && doorPostureTask_->eval().norm() < 0.01)
   {
     removeContact({"ur5e", "door", "Tool", "Handle"});
     dingoEndEffectorTask_->reset();
@@ -76,30 +67,23 @@ bool MobileArmController::run()
   return mc_control::MCController::run();
 }
 
-void MobileArmController::reset(const mc_control::ControllerResetData &reset_data) 
+void MobileArmController::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::MCController::reset(reset_data);
-  robots().robot(0).posW(
-    sva::PTransformd(sva::RotZ(0.0), Eigen::Vector3d(0.0, 0.0, 0.5)));
-  robots().robot(1).posW(
-    sva::PTransformd(sva::RotZ(0.0), Eigen::Vector3d(-0.25, 0.0, 0.0)));
-  robots().robot(2).posW(
-    sva::PTransformd(sva::RotZ(M_PI), Eigen::Vector3d(2.0, 1.0, 0)));
+  robots().robot(0).posW(sva::PTransformd(sva::RotZ(0.0), Eigen::Vector3d(0.0, 0.0, 0.5)));
+  robots().robot(1).posW(sva::PTransformd(sva::RotZ(0.0), Eigen::Vector3d(-0.25, 0.0, 0.0)));
+  robots().robot(2).posW(sva::PTransformd(sva::RotZ(M_PI), Eigen::Vector3d(2.0, 1.0, 0)));
 
-  handTask_ = 
-    std::make_shared<mc_tasks::SurfaceTransformTask>("Tool", robots(), 0);
-  dingoEndEffectorTask_ =
-    std::make_shared<mc_tasks::EndEffectorTask>("base_link", robots(), 1);
+  handTask_ = std::make_shared<mc_tasks::SurfaceTransformTask>("Tool", robots(), 0);
+  dingoEndEffectorTask_ = std::make_shared<mc_tasks::EndEffectorTask>("base_link", robots(), 1);
   dingoEndEffectorTask_->positionTask->stiffness(1.0);
   dingoEndEffectorTask_->positionTask->weight(1000.0);
   dingoEndEffectorTask_->orientationTask->stiffness(1.0);
   dingoEndEffectorTask_->orientationTask->weight(1000.0);
 
-  doorKinematics_ = 
-    std::make_shared<mc_solver::KinematicsConstraint>(robots(), 2, solver().dt());
+  doorKinematics_ = std::make_shared<mc_solver::KinematicsConstraint>(robots(), 2, solver().dt());
   solver().addConstraintSet(*doorKinematics_);
-  doorPostureTask_ =
-    std::make_shared<mc_tasks::PostureTask>(solver(), 2, 1.0, 1.0);
+  doorPostureTask_ = std::make_shared<mc_tasks::PostureTask>(solver(), 2, 1.0, 1.0);
   solver().addTask(doorPostureTask_);
 
   solver().addTask(dingoEndEffectorTask_);
--- doc/_includes/tutorials/introduction/mobile-arm-controller/controller.h
+++ doc/_includes/tutorials/introduction/mobile-arm-controller/controller.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <mc_control/mc_controller.h>
-#include <mc_tasks/SurfaceTransformTask.h>
 #include <mc_tasks/EndEffectorTask.h>
+#include <mc_tasks/SurfaceTransformTask.h>
 
 #include "api.h"
 
@@ -16,16 +16,16 @@ enum Phase
 
 struct MobileArmController_DLLAPI MobileArmController : public mc_control::MCController
 {
-  public:
-    MobileArmController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
-    bool run() override;
-    void reset(const mc_control::ControllerResetData & reset_data) override;
+public:
+  MobileArmController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
+  bool run() override;
+  void reset(const mc_control::ControllerResetData & reset_data) override;
 
-  private:
-    int count = 0;
-    Phase phase_ = APPROACH;
-    std::shared_ptr<mc_tasks::SurfaceTransformTask> handTask_;
-    std::shared_ptr<mc_tasks::EndEffectorTask> dingoEndEffectorTask_;
-    std::shared_ptr<mc_solver::KinematicsConstraint> doorKinematics_;
-    std::shared_ptr<mc_tasks::PostureTask> doorPostureTask_;
+private:
+  int count = 0;
+  Phase phase_ = APPROACH;
+  std::shared_ptr<mc_tasks::SurfaceTransformTask> handTask_;
+  std::shared_ptr<mc_tasks::EndEffectorTask> dingoEndEffectorTask_;
+  std::shared_ptr<mc_solver::KinematicsConstraint> doorKinematics_;
+  std::shared_ptr<mc_tasks::PostureTask> doorPostureTask_;
 };
