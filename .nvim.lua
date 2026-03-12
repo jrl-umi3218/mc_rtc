c532a89b04f84da2b9c8ffda63118d0d33f6d0de
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-03-12 16:19:39 +0100

    feat(lsp): Improve nvim yaml lsp configuration

--- .nvim.lua
+++ .nvim.lua
@@ -9,38 +9,49 @@ local file_dir = vim.fn.fnamemodify(file_path, ":h")
 print(file_dir)
 
 local function schema_path(rel)
-  local schema_dir = "file://" .. file_dir .. "/" .. schema_dir .. rel
-  print(schema_dir)
-  return schema_dir
+  local rel_schema_dir = "file://" .. file_dir .. "/" .. schema_dir .. rel
+  -- print(rel_schema_dir)
+  return rel_schema_dir
+end
+
+local raw_schemas = {
+  ["mc_rtc/mc_rtc.json"] = {
+    "**/mc_rtc.yaml",
+    "**/mc_rtc.in.yaml",
+  },
+  ["mc_control/FSMStates.json"] = {
+    "src/mc_control/fsm/states/data/*.yaml",
+  },
+  ["mc_control/FSMController.json"] = {
+    "src/mc_control/samples/Admittance/etc/AdmittanceSample.in.yaml",
+    "src/mc_control/samples/Door/etc/DoorSample.in.yaml",
+    "src/mc_control/samples/ExternalForces/etc/ExternalForces.in.yaml",
+    "src/mc_control/samples/LIPMStabilizer/etc/LIPMStabilizer.in.yaml",
+    "tests/controllers/TestFSMMetaContinuity.in.yaml",
+    "tests/controllers/TestFSMStateOptions.in.yaml",
+    "tests/controllers/TestPythonState.in.yaml",
+    "tests/controllers/TestObserverConfigurationController.in.yaml",
+  },
+}
+
+-- Transform schema paths to absolute paths to local schemas
+local schemas = {}
+for schema, patterns in pairs(raw_schemas) do
+  schemas[schema_path(schema)] = patterns
 end
 
 vim.lsp.config("yamlls", {
   settings = {
     yaml = {
-      schemas = {
-        [schema_path("mc_rtc/mc_rtc.json")] = {
-          "**/mc_rtc.yaml",
-          "**/mc_rtc.in.yaml",
-        },
-        [schema_path("mc_control/FSMStates.json")] = {
-          "src/mc_control/fsm/states/data/*.yaml",
-        },
-        [schema_path("mc_control/FSMController.json")] = {
-          "src/mc_control/samples/Admittance/etc/AdmittanceSample.in.yaml",
-          "src/mc_control/samples/Door/etc/DoorSample.in.yaml",
-          "src/mc_control/samples/ExternalForces/etc/ExternalForces.in.yaml",
-          "src/mc_control/samples/LIPMStabilizer/etc/LIPMStabilizer.in.yaml",
-          "tests/controllers/TestFSMMetaContinuity.in.yaml",
-          "tests/controllers/TestFSMStateOptions.in.yaml",
-          "tests/controllers/TestPythonState.in.yaml",
-          "tests/controllers/TestObserverConfigurationController.in.yaml",
-        },
-        validate = true,
-        format = { enable = false },
-        hover = true,
-        completion = true,
-      },
+      schemas = schemas,
+      validate = true,
+      format = { enable = false },
+      hover = true,
+      completion = true,
     },
   },
 })
-vim.cmd("LspRestart")
+
+if vim.fn.exists(':LspRestart') == 2 then
+  vim.cmd("LspRestart")
+end
