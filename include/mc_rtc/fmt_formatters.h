e93bd547881b3e858ecee82820b2c4bdf8f1154f
TANGUY Arnaud <arn.tanguy@gmail.com>
2026-03-13 23:13:05 +0100

    feat(fmt): Format filesystem, remove unused eigen code

--- include/mc_rtc/fmt_formatters.h
+++ include/mc_rtc/fmt_formatters.h
@@ -13,20 +13,8 @@
 #if FMT_VERSION >= 9 * 10000
 #  define MC_FMT_STREAMED(X) fmt::streamed(X)
 #  include <boost/filesystem.hpp>
-#  include <Eigen/Core>
+#  include <filesystem>
 #  include <fmt/ostream.h>
-#  include <fmt/ranges.h>
-#  include <type_traits>
-
-// Formatter for Eigen dense types (like Eigen::Matrix, Eigen::Array)
-template<typename T, typename Char>
-struct fmt::formatter<T,
-                      Char,
-                      std::enable_if_t<std::is_base_of_v<Eigen::DenseBase<T>, T>
-                                       && (fmt::range_format_kind<T, Char, void>::value == fmt::range_format::disabled)>>
-: fmt::ostream_formatter
-{
-};
 
 // Formatter for boost::filesystem::path
 template<>
@@ -34,6 +22,17 @@ struct fmt::formatter<boost::filesystem::path> : fmt::formatter<std::string>
 {
   template<typename FormatContext>
   auto format(const boost::filesystem::path & p, FormatContext & ctx)
+  {
+    return fmt::formatter<std::string>::format(p.string(), ctx);
+  }
+};
+
+// Formatter for std::filesystem::path
+template<>
+struct fmt::formatter<std::filesystem::path> : fmt::formatter<std::string>
+{
+  template<typename FormatContext>
+  auto format(const std::filesystem::path & p, FormatContext & ctx)
   {
     return fmt::formatter<std::string>::format(p.string(), ctx);
   }
